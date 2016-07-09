1e418993c351c9e27ba899964a3bcd7d5b809975
Shamasis Bhattacharya <mail@shamasis.net>
2016-07-10 03:33:26 +0530

    Remove integration tests that are not fully automated yet.

--- test/integration/miley-cyrus/assets/main.js
+++
@@ -1,6 +0,0 @@
-var runner = require('../../../../index');
-
-window.Runner = runner.Runner;
-window.Requester = runner.Requester;
-window.sdk = require('postman-collection');
-window.uvm = runner.uvm;
