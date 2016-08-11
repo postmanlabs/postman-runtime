cdc63e01af7645f2fb720add2532e37887de5f8d
Shamasis Bhattacharya <mail@shamasis.net>
2016-08-11 15:39:59 +0530

    Added iteration delay test

---
+++ test/integration-sinon/wait-state-change-event-iteration.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+    delay: {
+        iteration: 200
+    },
+    iterationCount: 2,
+
+    collection: {
+        item: [{
+            request: "https://echo.getpostman.com/get"
+        }]
+    }
+}
