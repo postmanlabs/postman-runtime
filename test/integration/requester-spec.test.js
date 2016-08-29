2c64ff32accf4d8b9e7128accb316abbb2c77052
Shamasis Bhattacharya <mail@shamasis.net>
2016-08-29 18:32:51 +0530

    Reorganised code of fileResolver usage in request command extension of runner. This also now ensures that request's properties are directly mutated.

--- test/integration/requester-spec.test.js
+++ test/integration/requester-spec.test.js
@@ -1699,10 +1699,10 @@ describe('Requester', function () {
 
                 expect(err).to.be(null);
                 run.start({
-                    console: function (level, message) {
+                    console: function (cursor, level, message) {
                         check(function () {
                             expect(level).to.be('warn');
-                            expect(message).to.be('Unable to load file for upload: /some/path');
+                            expect(message).to.be('unable to load form file for upload: "/some/path"');
                         });
                     },
                     start: function (err, cursor) {
@@ -2535,9 +2535,10 @@ describe('Requester', function () {
 
                 expect(err).to.be(null);
                 run.start({
-                    console: function () {
+                    console: function (cursor, level, message) {
                         check(function () {
-                            throw new Error('Console should not be called!');
+                            expect(level).be('warn');
+                            expect(message).be('unable to load raw file for upload: "undefined"');
                         });
                     },
                     start: function (err, cursor) {
