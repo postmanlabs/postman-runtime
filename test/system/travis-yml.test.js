018cedd4c54f7ed274c63adccd24668262d95d27
Aniket Panse <aniket@getpostman.com>
2016-09-11 17:27:22 +0530

    updated travis config to not contain node v5

--- test/system/travis-yml.test.js
+++ test/system/travis-yml.test.js
@@ -29,7 +29,7 @@ describe('travis.yml', function () {
     describe('strucure', function () {
         it('language must be set to node', function () {
             expect(travisYAML.language).to.be('node_js');
-            expect(travisYAML.node_js).to.eql(['4', '5', '6']);
+            expect(travisYAML.node_js).to.eql(['4', '6']);
         });
 
         it.skip('node version must match rest of the tests', function () {
