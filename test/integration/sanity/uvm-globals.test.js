dea20cff2fc801c8bd4cc6b80b0672a76b16584f
Aniket Panse <aniket@getpostman.com>
2016-10-03 13:06:32 +0530

    adding a test for uvm globals

---
+++ test/integration/sanity/uvm-globals.test.js
@@ -0,0 +1,49 @@
+describe('UVM allowed globals', function () {
+    var testrun;
+
+    before(function (done) {
+        this.run({
+            collection: {
+                item: {
+                    name: "r3",
+                    event: [{
+                        listen: "test",
+                        script: {
+                            type: "text/javascript",
+                            exec: "console.log(Object.keys(this));"
+                        }
+                    }],
+                    request: 'httpbin.org/get'
+                }
+            },
+            environment: { testVar: 'test-var-value' }
+        }, function (err, results) {
+            testrun = results;
+            done(err);
+        });
+    });
+
+    it('must have started and completed the test run', function () {
+        expect(testrun).be.ok();
+        expect(testrun.done.calledOnce).be.ok();
+        expect(testrun.start.calledOnce).be.ok();
+    });
+
+    it('must resolve variable and send request', function () {
+        var request = testrun.beforeRequest.getCall(0).args[2];
+
+        expect(testrun.beforeRequest.calledOnce).be.ok(); // one request
+        expect(request).be.ok();
+        expect(request.url.toString()).eql('https://echo.getpostman.com/get?testvar=test-var-value');
+        expect(request.method).be('GET');
+    });
+
+    it('must receive response with the query param sent', function () {
+        var response = testrun.request.getCall(0).args[2];
+
+        expect(testrun.request.calledOnce).be.ok(); // one request
+        expect(response.json()).be.ok();
+        expect(response.json().args).be.ok();
+        expect(response.json().args).have.property('testvar', 'test-var-value');
+    });
+});
