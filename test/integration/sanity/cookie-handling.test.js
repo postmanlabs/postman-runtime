485d09d9f1e5e49294e5ad19c9c27636fbf8598d
Aniket Panse <aniket@getpostman.com>
2016-11-30 15:41:38 +0530

    Fixed cookieJar defaulting to undefined, added a test for it too

---
+++ test/integration/sanity/cookie-handling.test.js
@@ -0,0 +1,39 @@
+describe('sanity test', function () {
+    var _ = require('lodash'),
+        testrun;
+
+    before(function (done) {
+        this.run({
+            collection: {
+                item: [{
+                    event: [{
+                        listen: 'test',
+                        script: {
+                            type: 'text/javascript',
+                            exec: ['tests["working"] = postman.getResponseCookie("foo").value === "bar"']
+                        }
+                    }],
+                    request: 'http://echo.getpostman.com/cookies/set?foo=bar'
+                }]
+            }
+        }, function (err, results) {
+            testrun = results;
+            done(err);
+        });
+    });
+
+    it('must have run the test script successfully', function () {
+        expect(testrun).be.ok();
+        expect(testrun.test.calledOnce).be.ok();
+
+        expect(testrun.test.getCall(0).args[0]).to.be(null);
+        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests.working')).to.be(true);
+    });
+
+    it('must have completed the run', function () {
+        expect(testrun).be.ok();
+        expect(testrun.done.calledOnce).be.ok();
+        expect(testrun.done.getCall(0).args[0]).to.be(null);
+        expect(testrun.start.calledOnce).be.ok();
+    });
+});
