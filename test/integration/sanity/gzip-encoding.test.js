e071fa45c97959007e7df49280695043e13bc63e
kunagpal <kunal@getpostman.com>
2017-03-28 20:03:06 +0530

    Added gzip encoding integration tests

---
+++ test/integration/sanity/gzip-encoding.test.js
@@ -0,0 +1,41 @@
+describe('GZIP encoding', function() {
+    var _ = require('lodash'),
+        testrun;
+
+    before(function(done) {
+        this.run({
+            collection: {
+                item: [{
+                    event: [{
+                        listen: 'test',
+                        script: {
+                            exec: 'tests["working"] = JSON.parse(responseBody).gzipped;'
+                        }
+                    }],
+                    request: {
+                        url: 'http://postman-echo.com/gzip',
+                        method: 'GET'
+                    }
+                }]
+            }
+        }, function(err, results) {
+            testrun = results;
+            done(err);
+        });
+    });
+
+    it('must have run the test script successfully', function() {
+        expect(testrun).be.ok();
+        expect(testrun.test.calledOnce).be.ok();
+
+        expect(testrun.test.getCall(0).args[0]).to.be(null);
+        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests.working')).to.be(true);
+    });
+
+    it('must have completed the run', function() {
+        expect(testrun).be.ok();
+        expect(testrun.done.calledOnce).be.ok();
+        expect(testrun.done.getCall(0).args[0]).to.be(null);
+        expect(testrun.start.calledOnce).be.ok();
+    });
+});
