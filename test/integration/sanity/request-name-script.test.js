29106a2362cb17aea6672c0c7348745207f82084
kunagpal <kunal@getpostman.com>
2017-03-28 19:56:38 +0530

    Added request name script integrationt tests

---
+++ test/integration/sanity/request-name-script.test.js
@@ -0,0 +1,53 @@
+describe('Request name scripts', function() {
+    var _ = require('lodash'),
+        testrun;
+
+    before(function(done) {
+        this.run({
+            collection: {
+                item: [{
+                    name: 'r1',
+                    event: [{
+                        listen: 'test',
+                        script: {
+                            exec: 'tests[\'working\'] = (request.name===\'r1\' && request.description===\'testDesc\')'
+                        }
+                    }, {
+                        listen: 'prerequest',
+                        script: {
+                            exec: 'if(request.name!==\'r1\' || request.description!==\'testDesc\') {undefFunction()}'
+                        }
+                    }],
+                    request: {
+                        url: 'postman-echo.com/get',
+                        method: 'GET',
+                        body: {
+                            mode: 'formdata',
+                            formdata: []
+                        },
+                        description: 'testDesc'
+                    }
+                }]
+            }
+        }, function(err, results) {
+            testrun = results;
+            done(err);
+        });
+    });
+
+    it('must have run the test script successfully', function() {
+        expect(testrun).be.ok();
+        expect(testrun.test.calledOnce).be.ok();
+        expect(testrun.prerequest.calledOnce).be.ok();
+
+        expect(testrun.test.getCall(0).args[0]).to.be(null);
+        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests.working')).to.be(true);
+    });
+
+    it('must have completed the run', function() {
+        expect(testrun).be.ok();
+        expect(testrun.done.calledOnce).be.ok();
+        expect(testrun.done.getCall(0).args[0]).to.be(null);
+        expect(testrun.start.calledOnce).be.ok();
+    });
+});
