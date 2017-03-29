40ebf14ff2f422df7aaf94b2ea448a966ead8852
Aniket Panse <aniketpanse@gmail.com>
2017-03-29 18:49:19 +0530

    do not rely on sandbox to check redirect behavior

--- test/integration/sanity/redirect.test.js
+++ test/integration/sanity/redirect.test.js
@@ -1,20 +1,12 @@
-describe('redirets', function() {
-    var _ = require('lodash'),
-        testrun;
+describe('redirects', function() {
+    var testrun;
 
     before(function(done) {
         this.run({
             requester: {followRedirects: false},
             collection: {
                 item: [{
-                    event: [{
-                        listen: 'test',
-                        script: {exec: 'tests[\'working\'] = responseCode.code === 302;'}
-                    }],
-                    request: {
-                        url: 'https://postman-echo.com/redirect-to?url=https://postman-echo.com/get',
-                        method: 'GET'
-                    }
+                    request: 'https://postman-echo.com/redirect-to?url=https://postman-echo.com/get'
                 }]
             }
         }, function(err, results) {
@@ -23,12 +15,17 @@ describe('redirets', function() {
         });
     });
 
-    it('must have run the test script successfully', function() {
+    it('must have sent the request successfully', function() {
         expect(testrun).be.ok();
-        expect(testrun.test.calledOnce).be.ok();
+        expect(testrun.request.calledOnce).be.ok();
 
-        expect(testrun.test.getCall(0).args[0]).to.be(null);
-        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests.working')).to.be(true);
+        expect(testrun.request.getCall(0).args[0]).to.be(null);
+    });
+
+    it('must not have followed the redirect', function() {
+        var response = testrun.request.getCall(0).args[2];
+
+        expect(response.code).to.eql(302);
     });
 
     it('must have completed the run', function() {
