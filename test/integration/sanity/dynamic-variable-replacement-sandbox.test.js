1adfd0f90d0029c6f92be91873c7e2f45b08c3e5
Aniket Panse <aniketpanse@gmail.com>
2017-03-29 16:07:19 +0530

    rewrote the dynamic var replacement tests

--- test/integration/sanity/dynamic-variable-replacement-sandbox.test.js
+++ test/integration/sanity/dynamic-variable-replacement-sandbox.test.js
@@ -1,56 +1,149 @@
-describe('Dynamic variable replacement', function () {
-    var _ = require('lodash'),
-        testrun;
+describe('Dynamic Variables', function () {
+    var _ = require('lodash');
 
-    before(function (done) {
-        this.run({
-            collection: {
-                item: [{
-                    event: [{
-                        listen: 'test',
-                        script: {
-                            exec: ['var responseJSON;',
-                                'try { responseJSON = JSON.parse(responseBody); }',
-                                'catch (e) { console.log(e); }',
-                                'tests["FriendlyName should be a GUID"] = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/i.test(responseJSON.form["FriendlyName"]);',
-                                'tests["RandomId should be an integer"] = (_.parseInt(responseJSON.form["RandomId"]) % 1) === 0;',
-                                'tests["TimeCreated should be an integer"] = (_.parseInt(responseJSON.form["TimeCreated"]) % 1) === 0;']
+    describe('{{$guid}}', function () {
+        var testrun;
+
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: 'https://postman-echo.com/post',
+                            method: 'POST',
+                            body: {
+                                mode: 'urlencoded',
+                                urlencoded: [
+                                    {key: 'FriendlyName', value: '{{$guid}}', type: 'text', enabled: true}
+                                ]
+                            }
                         }
-                    }],
-                    request: {
-                        url: 'https://postman-echo.com/post',
-                        method: 'POST',
-                        body: {
-                            mode: 'urlencoded',
-                            urlencoded: [{key: 'FriendlyName', value: '{{$guid}}', type: 'text', enabled: true},
-                                {key: 'RandomId', value: '{{$randomInt}}', type: 'text', enabled: true},
-                                {key: 'TimeCreated', value: '{{$timestamp}}', type: 'text', enabled: true}]
-                        }
-                    }
-                }]
-            }
-        }, function (err, results) {
-            testrun = results;
-            done(err);
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have sent the request successfully', function () {
+            expect(testrun).be.ok();
+            expect(testrun.request.calledOnce).be.ok();
+            expect(testrun.request.getCall(0).args[0]).to.be(null);
+        });
+
+        it('must have replaced the GUID variable', function () {
+            var sent = testrun.request.getCall(0).args[3],
+                param;
+
+            expect(sent.body.urlencoded).to.be.ok();
+
+            param = sent.body.urlencoded.idx(0);
+            expect(param.value)
+                .to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/i);
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
         });
     });
 
-    it('must have run the test script successfully', function () {
-        expect(testrun).be.ok();
-        expect(testrun.test.calledOnce).be.ok();
+    describe('{{$randomInt}}', function () {
+        var testrun;
 
-        expect(testrun.test.getCall(0).args[0]).to.be(null);
-        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests')).to.eql({
-            'FriendlyName should be a GUID': true,
-            'RandomId should be an integer': true,
-            'TimeCreated should be an integer': true
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: 'https://postman-echo.com/post',
+                            method: 'POST',
+                            body: {
+                                mode: 'urlencoded',
+                                urlencoded: [{key: 'RandomId', value: '{{$randomInt}}', type: 'text', enabled: true}]
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have sent the request successfully', function () {
+            expect(testrun).be.ok();
+            expect(testrun.request.calledOnce).be.ok();
+            expect(testrun.request.getCall(0).args[0]).to.be(null);
+        });
+
+        it('must have replaced the randomInt variable', function () {
+            var sent = testrun.request.getCall(0).args[3],
+                param;
+
+            expect(sent.body.urlencoded).to.be.ok();
+
+            param = sent.body.urlencoded.idx(0);
+            param = _.parseInt(param.value, 10);
+            expect(_.isInteger(param)).to.be(true);
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
         });
     });
 
-    it('must have completed the run', function () {
-        expect(testrun).be.ok();
-        expect(testrun.done.calledOnce).be.ok();
-        expect(testrun.done.getCall(0).args[0]).to.be(null);
-        expect(testrun.start.calledOnce).be.ok();
+    describe('{{$timestamp}}', function () {
+        var testrun;
+
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: 'https://postman-echo.com/post',
+                            method: 'POST',
+                            body: {
+                                mode: 'urlencoded',
+                                urlencoded: [{key: 'TimeCreated', value: '{{$timestamp}}', type: 'text', enabled: true}]
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have sent the request successfully', function () {
+            expect(testrun).be.ok();
+            expect(testrun.request.calledOnce).be.ok();
+            expect(testrun.request.getCall(0).args[0]).to.be(null);
+        });
+
+        it('must have replaced the timestamp variable', function () {
+            var sent = testrun.request.getCall(0).args[3],
+                param;
+
+            expect(sent.body.urlencoded).to.be.ok();
+
+            param = sent.body.urlencoded.idx(0);
+            param = _.parseInt(param.value, 10);
+            expect(_.isInteger(param)).to.be(true);
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
+        });
     });
 });
