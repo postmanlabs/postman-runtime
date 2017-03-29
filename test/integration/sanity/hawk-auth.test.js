4ce2e0da088d1fd0b295865efcf7b8465fe4bb35
Aniket Panse <aniketpanse@gmail.com>
2017-03-29 16:37:51 +0530

    fixed hawk auth tests

--- test/integration/sanity/hawk-auth.test.js
+++ test/integration/sanity/hawk-auth.test.js
@@ -1,4 +1,4 @@
-describe('HAWK authentication', function () {
+describe('Hawk authentication', function () {
     var _ = require('lodash'),
         testrun;
 
@@ -6,13 +6,6 @@ describe('HAWK authentication', function () {
         this.run({
             collection: {
                 item: [{
-                    event: [{
-                        listen: 'test',
-                        script: {
-                            exec: ['var response = JSON.parse(responseBody);',
-                                'tests["working"] = response.status === "pass";']
-                        }
-                    }],
                     request: {
                         auth: {
                             type: 'hawk',
@@ -31,10 +24,6 @@ describe('HAWK authentication', function () {
                         },
                         url: 'http://postman-echo.com/auth/hawk',
                         method: 'GET',
-                        header: [{
-                            key: 'Authorization',
-                            value: 'Hawk id=\'dh37fgj492je\', ts=\'1448888081\', nonce=\'HoH6Ay\', ext=\'skjdfklsjhdflkjhsdf\', mac=\'moWleO5f/8QbvIiy7oo2zj1bmezhrYwrCkz4BsXg0M4=\''
-                        }]
                     }
                 }]
             }
@@ -44,12 +33,15 @@ describe('HAWK authentication', function () {
         });
     });
 
-    it('must have run the test script successfully', function () {
+    it('must have authorized successfully', function () {
         expect(testrun).be.ok();
-        expect(testrun.test.calledOnce).be.ok();
+        expect(testrun.request.calledOnce).be.ok();
 
-        expect(testrun.test.getCall(0).args[0]).to.be(null);
-        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests.working')).to.be(true);
+        expect(testrun.request.getCall(0).args[0]).to.be(null);
+
+        var response = testrun.request.getCall(0).args[2];
+        expect(response.code).to.eql(200);
+        expect(JSON.parse(response.body).status).to.eql('pass');
     });
 
     it('must have completed the run', function () {
