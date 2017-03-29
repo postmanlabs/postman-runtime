2ea59c97e3160d82d8f75ca5edff623bbe1a9990
kunagpal <kunal@getpostman.com>
2017-03-29 21:51:43 +0530

    Fixed postman.setNextRequest integration tests

--- test/integration/sanity/set-next-request.test.js
+++ test/integration/sanity/set-next-request.test.js
@@ -6,63 +6,27 @@ describe('Set next request', function() {
         this.run({
             collection: {
                 item: [{
-                    name: 'post',
+                    name: 'one',
+                    request: {
+                        url: 'https://postman-echo.com/get?name=one',
+                        method: 'GET'
+                    },
                     event: [{
                         listen: 'test',
                         script: {
-                            type: 'text/javascript',
-                            exec: [
-                                'postman.setEnvironmentVariable("method", "get");',
-                                'postman.setEnvironmentVariable("count", "1");',
-                                'postman.setNextRequest("method");'
-                            ]
+                            exec: ['postman.setNextRequest("three");']
                         }
-                    }],
-                    request: {
-                        url: 'https://postman-echo.com/post',
-                        method: 'POST'
-                    }
+                    }]
                 }, {
-                    event: [{
-                        listen: 'test',
-                        script: {
-                            exec: [
-                                'var count = _.parseInt(postman.getEnvironmentVariable("count"));',
-                                'count++;',
-                                'postman.setEnvironmentVariable("count", String(count));',
-                                'if (responseCode.code === 200) {',
-                                '    postman.setEnvironmentVariable("method", "headers");',
-                                '    postman.setNextRequest("method");',
-                                '}'
-                            ]
-                        }
-                    }],
+                    name: 'two',
                     request: {
-                        url: 'https://postman-echo.com/type/html',
+                        url: 'https://postman-echo.com/get?name=two',
                         method: 'GET'
                     }
                 }, {
-                    name: 'method',
-                    event: [{
-                        listen: 'test',
-                        script: {
-                            exec: [
-                                'var jsonData = JSON.parse(responseBody);',
-                                'var count = _.parseInt(postman.getEnvironmentVariable("count"));',
-                                'count++;',
-                                'postman.setEnvironmentVariable("count", String(count));',
-                                'if (jsonData.url === "https://postman-echo.com/get") {',
-                                '    postman.setNextRequest("html");',
-                                '}',
-                                'else if (!jsonData.url && jsonData.headers) {',
-                                'tests["Success"] = _.parseInt(postman.getEnvironmentVariable("count")) === 4',
-                                '    postman.setNextRequest(null);',
-                                '}'
-                            ]
-                        }
-                    }],
+                    name: 'three',
                     request: {
-                        url: 'https://postman-echo.com/{{method}}',
+                        url: 'https://postman-echo.com/get?name=three',
                         method: 'GET'
                     }
                 }]
@@ -75,13 +39,10 @@ describe('Set next request', function() {
 
     it('must have run the test script successfully', function() {
         expect(testrun).be.ok();
-        expect(testrun.test.calledTwice).be.ok();
+        expect(testrun.request.calledTwice).be.ok();
 
-        expect(testrun.test.getCall(0).args[0]).to.be(null);
-        expect(_.get(testrun.test.getCall(0).args[2], '0.result.globals.tests')).to.eql({});
-
-        expect(testrun.test.getCall(1).args[0]).to.be(null);
-        expect(_.get(testrun.test.getCall(1).args[2], '0.result.globals.tests')).to.eql({});
+        expect(_.get(testrun.request.getCall(0).args[6], 'name')).to.be('one');
+        expect(_.get(testrun.request.getCall(1).args[6], 'name')).to.be('three');
     });
 
     it('must have completed the run', function() {
