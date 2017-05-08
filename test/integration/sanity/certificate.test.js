6842503415c7368daf8bb386e24d82ef51aaccca
Aniket Panse <aniketpanse@gmail.com>
2017-05-08 14:26:12 +0530

    added a test for invalid cert paths

--- test/integration/sanity/certificate.test.js
+++ test/integration/sanity/certificate.test.js
@@ -9,79 +9,159 @@ describe('certificates', function () {
         server,
         testrun;
 
-    before(function (done) {
-        var port = 9090,
-            certDataPath = path.join(__dirname, '..', '..', 'integration-legacy', 'data'),
-            clientKeyPath = path.join(certDataPath, 'client1-key.pem'),
-            clientCertPath = path.join(certDataPath, 'client1-crt.pem'),
+    describe('valid', function () {
+        before(function (done) {
+            var port = 9090,
+                certDataPath = path.join(__dirname, '..', '..', 'integration-legacy', 'data'),
+                clientKeyPath = path.join(certDataPath, 'client1-key.pem'),
+                clientCertPath = path.join(certDataPath, 'client1-crt.pem'),
 
-            serverKeyPath = path.join(certDataPath, 'server-key.pem'),
-            serverCertPath = path.join(certDataPath, 'server-crt.pem'),
-            serverCaPath = path.join(certDataPath, 'ca-crt.pem'),
+                serverKeyPath = path.join(certDataPath, 'server-key.pem'),
+                serverCertPath = path.join(certDataPath, 'server-crt.pem'),
+                serverCaPath = path.join(certDataPath, 'ca-crt.pem'),
 
-            certificateList = new CertificateList({}, [{
-                id: certificateId,
-                matches: ['https://localhost:' + port + '/*'],
-                key: {src: clientKeyPath},
-                cert: {src: clientCertPath}
-            }]);
+                certificateList = new CertificateList({}, [{
+                    id: certificateId,
+                    matches: ['https://localhost:' + port + '/*'],
+                    key: { src: clientKeyPath },
+                    cert: { src: clientCertPath }
+                }]);
 
-        server = https.createServer({
-            key: fs.readFileSync(serverKeyPath),
-            cert: fs.readFileSync(serverCertPath),
-            ca: fs.readFileSync(serverCaPath),
-            requestCert: true
-        });
+            server = https.createServer({
+                key: fs.readFileSync(serverKeyPath),
+                cert: fs.readFileSync(serverCertPath),
+                ca: fs.readFileSync(serverCaPath),
+                requestCert: true
+            });
 
-        server.on('request', function (req, res) {
-            if (req.client.authorized) {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end('authorized\n');
-            }
-            else {
-                res.writeHead(401, {'Content-Type': 'text/plain'});
-                res.end('unauthorized\n');
-            }
-        });
-
-        server.listen(port, 'localhost');
-
-        this.run({
-            collection: {
-                item: {
-                    request: 'https://localhost:' + port + '/'
+            server.on('request', function (req, res) {
+                if (req.client.authorized) {
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end('authorized\n');
                 }
-            },
-            requester: {
-                strictSSL: false
-            },
-            fileResolver: fs,
-            certificates: certificateList
-        }, function (err, results) {
-            testrun = results;
-            done(err);
+                else {
+                    res.writeHead(401, { 'Content-Type': 'text/plain' });
+                    res.end('unauthorized\n');
+                }
+            });
+
+            server.listen(port, 'localhost');
+
+            this.run({
+                collection: {
+                    item: {
+                        request: 'https://localhost:' + port + '/'
+                    }
+                },
+                requester: {
+                    strictSSL: false
+                },
+                fileResolver: fs,
+                certificates: certificateList
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have started and completed the test run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.start.calledOnce).be.ok();
+        });
+
+        it('must receive response from https server', function () {
+            var response = testrun.request.getCall(0).args[2];
+
+            expect(response.text()).to.eql('authorized\n');
+        });
+
+        it('must have certificate attached to request', function () {
+            var request = testrun.request.getCall(0).args[3].toJSON();
+
+            expect(request.certificate.id).to.eql(certificateId);
+        });
+
+        after(function () {
+            server.close();
         });
     });
 
-    it('must have started and completed the test run', function () {
-        expect(testrun).be.ok();
-        expect(testrun.done.calledOnce).be.ok();
-        expect(testrun.start.calledOnce).be.ok();
-    });
+    describe.skip('invalid', function () {
+        before(function (done) {
+            var port = 9090,
+                certDataPath = path.join(__dirname, '..', '..', 'integration-legacy', 'data'),
+                clientKeyPath = path.join('/tmp/non-existent/', 'client1-key.pem'),
+                clientCertPath = path.join('/tmp/non-existent/', 'client1-crt.pem'),
 
-    it('must receive response from https server', function () {
-        var response = testrun.request.getCall(0).args[2];
+                serverKeyPath = path.join(certDataPath, 'server-key.pem'),
+                serverCertPath = path.join(certDataPath, 'server-crt.pem'),
+                serverCaPath = path.join(certDataPath, 'ca-crt.pem'),
 
-        expect(response.text()).to.eql('authorized\n');
-    });
+                certificateList = new CertificateList({}, [{
+                    id: certificateId,
+                    matches: ['https://localhost:' + port + '/*'],
+                    key: { src: clientKeyPath },
+                    cert: { src: clientCertPath }
+                }]);
 
-    it('must have certificate attached to request', function () {
-        var request = testrun.request.getCall(0).args[3].toJSON();
+            server = https.createServer({
+                key: fs.readFileSync(serverKeyPath),
+                cert: fs.readFileSync(serverCertPath),
+                ca: fs.readFileSync(serverCaPath),
+                requestCert: true
+            });
 
-        expect(request.certificate.id).to.eql(certificateId);
-    });
+            server.on('request', function (req, res) {
+                if (req.client.authorized) {
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end('authorized\n');
+                }
+                else {
+                    res.writeHead(401, { 'Content-Type': 'text/plain' });
+                    res.end('unauthorized\n');
+                }
+            });
 
-    after(function () {
-        server.close();
+            server.listen(port, 'localhost');
+
+            this.run({
+                collection: {
+                    item: {
+                        request: 'https://localhost:' + port + '/'
+                    }
+                },
+                requester: {
+                    strictSSL: false
+                },
+                fileResolver: fs,
+                certificates: certificateList
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have started and completed the test run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.start.calledOnce).be.ok();
+        });
+
+        it('must not throw an error', function () {
+            var response = testrun.request.getCall(0).args[2];
+
+            expect(response.text()).to.eql('authorized\n');
+        });
+
+        it('must have certificate attached to request', function () {
+            var request = testrun.request.getCall(0).args[3].toJSON();
+
+            expect(request.certificate.id).to.eql(certificateId);
+        });
+
+        after(function () {
+            server.close();
+        });
     });
 });
