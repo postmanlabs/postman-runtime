faff0fca05672f593f3cb2381692d11af3b597eb
Aniket Panse <aniketpanse@gmail.com>
2017-06-19 17:38:43 +0530

    allow sending requests from inside the sandbox

---
+++ test/integration/request-flow/requests-from-sandbox.test.js
@@ -0,0 +1,79 @@
+var sdk = require('postman-collection');
+
+describe('requests from sandbox', function() {
+    describe('sanity checks', function () {
+        var testrun,
+            sandboxRequestUrl = 'postman-echo.com/get?sandbox=true';
+
+        before(function(done) {
+            this.run({
+                collection: {
+                    item: {
+                        // ensure that we run something for test and pre-req scripts
+                        event: [{
+                            listen: 'prerequest',
+                            script: {
+                                exec: `
+                                var sdk = require('postman-collection'),
+                                    myreq = new sdk.Request('${sandboxRequestUrl}');
+                                
+                                pm.sendRequest(myreq, function(err, _response) {
+                                    pm.test('request was sent from sandbox', function () {
+                                        pm.expect(_response).to.have.property('code', 200);
+                                        pm.expect(_response).to.have.property('status', 'OK');
+                                    });
+                                });
+                                `
+                            }
+                        }],
+                        request: 'https://postman-echo.com/get'
+                    }
+                }
+            }, function(err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have called io event twice', function () {
+            expect(testrun.io.calledTwice).to.be(true);
+        });
+
+        it('should have sent the first request from inside the sandbox', function () {
+            var error = testrun.io.firstCall.args[0],
+                request = testrun.io.firstCall.args[4],
+                response = testrun.io.firstCall.args[3],
+                trace = testrun.io.firstCall.args[2];
+
+            expect(error).to.be(null);
+
+            expect(request.url.toString()).to.eql(sandboxRequestUrl);
+            expect(response.code).to.eql(200);
+
+            expect(trace).to.have.property('type', 'http');
+            expect(trace).to.have.property('source', 'script');
+        });
+
+        it('should have sent the second request as a part of the collection run', function () {
+            var error = testrun.io.secondCall.args[0],
+                request = testrun.io.secondCall.args[4],
+                response = testrun.io.secondCall.args[3],
+                trace = testrun.io.secondCall.args[2];
+
+            expect(error).to.be(null);
+
+            expect(request.url.toString()).to.eql('https://postman-echo.com/get');
+            expect(response.code).to.eql(200);
+
+            expect(trace).to.have.property('type', 'http');
+            expect(trace).to.have.property('source', 'collection');
+        });
+
+        it('must have completed the run', function() {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
+        });
+    });
+});
