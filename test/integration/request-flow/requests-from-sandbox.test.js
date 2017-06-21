9946f50eb603f6886081d87467fb9f40f28f3337
Aniket Panse <aniketpanse@gmail.com>
2017-06-21 13:39:32 +0530

    added a test to ensure that requesting errors are provided inside the sandbox

--- test/integration/request-flow/requests-from-sandbox.test.js
+++ test/integration/request-flow/requests-from-sandbox.test.js
@@ -237,4 +237,102 @@ describe('requests from sandbox', function() {
             expect(testrun.start.calledOnce).be.ok();
         });
     });
+
+    describe('sending errors', function () {
+        var testrun,
+            sandboxRequestUrl = 'somenonexistentweirddomain.com/get?sandbox=true';
+
+        before(function(done) {
+            this.run({
+                collection: {
+                    item: {
+                        // ensure that we run something for test and pre-req scripts
+                        event: [{
+                            listen: 'prerequest',
+                            script: {
+                                exec: `
+                                var sdk = require('postman-collection'),
+                                    myreq = new sdk.Request('${sandboxRequestUrl}');
+                                
+                                pm.sendRequest(myreq, function(err, _response) {
+                                    pm.test('request did not complete', function () {
+                                        pm.expect(err).to.have.property('code', 'ENOTFOUND');
+                                        pm.expect(_response).to.not.be.ok;
+                                    });
+                                });
+                                `
+                            }
+                        }],
+                        request: 'https://postman-echo.com/get'
+                    }
+                }
+            }, function(err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have called io event twice', function () {
+            expect(testrun.io.calledTwice).to.be(true);
+        });
+
+        it('should have called the request event once', function () {
+            expect(testrun.request.calledOnce).to.be(true);
+        });
+
+        it('should have the same cursor id for both the io events', function () {
+            var ref = testrun.beforeItem.firstCall.args[1].ref;
+
+            expect(testrun.io.firstCall.args[1].ref).to.eql(ref);
+            expect(testrun.io.secondCall.args[1].ref).to.eql(ref);
+        });
+
+        it('should error out on the first request', function () {
+            var error = testrun.io.firstCall.args[0],
+                request = testrun.io.firstCall.args[4],
+                response = testrun.io.firstCall.args[3],
+                trace = testrun.io.firstCall.args[2];
+
+            expect(error).to.be.ok();
+            expect(error).to.have.property('code', 'ENOTFOUND');
+
+            expect(request.url.toString()).to.eql(sandboxRequestUrl);
+            expect(response).to.be(undefined);
+
+            expect(trace).to.have.property('type', 'http');
+            expect(trace).to.have.property('source', 'script');
+        });
+
+        it('should have provided the error to the sandbox sendrequest function', function () {
+            var assertion = testrun.assertion.firstCall.args[1];
+
+            expect(assertion).to.have.property('name', 'request did not complete');
+            expect(assertion).to.have.property('skipped', false);
+            expect(assertion).to.have.property('passed', true);
+            expect(assertion).to.have.property('error', null);
+            expect(assertion).to.have.property('index', 0);
+        });
+
+        it('should have sent the second request as a part of the collection run', function () {
+            var error = testrun.io.secondCall.args[0],
+                request = testrun.io.secondCall.args[4],
+                response = testrun.io.secondCall.args[3],
+                trace = testrun.io.secondCall.args[2];
+
+            expect(error).to.be(null);
+
+            expect(request.url.toString()).to.eql('https://postman-echo.com/get');
+            expect(response.code).to.eql(200);
+
+            expect(trace).to.have.property('type', 'http');
+            expect(trace).to.have.property('source', 'collection');
+        });
+
+        it('must have completed the run', function() {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
+        });
+    });
 });
