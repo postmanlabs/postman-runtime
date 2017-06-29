50c2759ff84c73b17a48bd7741db8439732ca014
Kunal Nagpal <kunagpal@gmail.com>
2017-06-29 20:13:47 +0530

    Added fake auth test 1

--- test/integration/auth-methods/fake.test.js
+++ test/integration/auth-methods/fake.test.js
@@ -3,49 +3,14 @@
 var _ = require('lodash'),
     sinon = require('sinon'),
     sdk = require('postman-collection'),
-    Authorizer = require('../../../lib/authorizer').Authorizer;
+    Authorizer = require('../../../lib/authorizer/index').Authorizer;
 
 describe('fake auth', function () {
-    var testrun,
-        fakeHandler = {
-            init: function (context, requester, done) {
-                done(null);
-            },
-
-            pre: function (context, requester, done) {
-                done(null, true);
-            },
-
-            post: function (context, requester, done) {
-                done(null, true);
-            },
-
-            _sign: function (request) {
-                return request;
-            }
+    var fakeSigner = {
+            update: function (params) { _.assign(this, params); },
+            authorize: function (request) { return request; }
         },
-        fakeSigner = {
-            update: function (params) {
-                _.assign(this, params);
-            },
-
-            authorize: function (request) {
-                return request;
-            }
-        },
-        handlerSpies = {
-            pre: sinon.spy(fakeHandler, 'pre'),
-            post: sinon.spy(fakeHandler, 'post'),
-            _sign: sinon.spy(fakeHandler, '_sign')
-        },
-        signerSpy = sinon.spy(fakeSigner, 'update');
-
-    before(function (done) {
-        sdk.RequestAuth.addType(fakeSigner, 'fake');
-        Authorizer.addHandler(fakeHandler, 'fake');
-
-        // perform the collection run
-        this.run({
+        runOptions = {
             collection: {
                 item: {
                     name: 'FakeAuth',
@@ -54,55 +19,74 @@ describe('fake auth', function () {
                         auth: {
                             type: 'fake',
                             fake: {
-                                username: '{{uname}}',
-                                password: '{{pass}}'
+                                username: 'postman',
+                                password: 'password'
                             }
                         }
                     }
                 }
             },
-            environment: {
-                values: [{
-                    key: 'uname',
-                    value: 'postman'
-                }, {
-                    key: 'pass',
-                    value: 'password'
-                }]
-            },
             authorizer: {interactive: true}
-        }, function (err, results) {
-            testrun = results;
-            done(err);
-        });
-    });
+        },
+        signerSpy = sinon.spy(fakeSigner, 'update');
 
+    before(function () {
+        sdk.RequestAuth.addType(fakeSigner, 'fake');
+    });
     after(function () {
-        Authorizer.removeHandler('fake');
         delete sdk.RequestAuth.types.fake; // todo: add a function in the SDK to remove an auth type.
     });
 
-    it('must have completed the run', function () {
-        expect(testrun).be.ok();
-        expect(testrun.done.calledOnce).be.ok();
-        testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
-        expect(testrun.done.getCall(0).args[0]).to.be(null);
-        expect(testrun.start.calledOnce).be.ok();
+    describe('working init, pre, and post', function () {
+        var testrun,
+            fakeHandler = {
+                init: function (context, requester, done) { done(null); },
+                pre: function (context, requester, done) { done(null, true); },
+                post: function (context, requester, done) { done(null, true); },
+                _sign: function (request) { return request; }
+            },
+            handlerSpies = {
+                pre: sinon.spy(fakeHandler, 'pre'),
+                post: sinon.spy(fakeHandler, 'post'),
+                _sign: sinon.spy(fakeHandler, '_sign')
+            };
 
-        // Authorizer flow related assertions
-        expect(handlerSpies.pre.calledOnce).to.be.ok();
-        expect(handlerSpies.post.calledOnce).to.be.ok();
-        expect(handlerSpies._sign.calledOnce).to.be.ok();
-        expect(signerSpy.calledThrice).to.be.ok();
-    });
+        before(function (done) {
+            Authorizer.addHandler(fakeHandler, 'fake');
+            // perform the collection run
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
 
-    it('must have sent the request once', function () {
-        expect(testrun.request.calledOnce).be.ok();
+        after(function () {
+            Authorizer.removeHandler('fake');
+            fakeSigner.update.reset();
+        });
 
-        var err = testrun.request.firstCall.args[0],
-            request = testrun.request.firstCall.args[3];
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.calledOnce).be.ok();
+            testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.calledOnce).be.ok();
 
-        expect(err).to.be(null);
-        expect(request.url.toString()).to.eql('https://postman-echo.com/basic-auth');
+            // Authorizer flow related assertions
+            expect(handlerSpies.pre.calledOnce).to.be.ok();
+            expect(handlerSpies.post.calledOnce).to.be.ok();
+            expect(handlerSpies._sign.calledOnce).to.be.ok();
+            expect(signerSpy.calledThrice).to.be.ok();
+        });
+
+        it('must have sent the request once', function () {
+            expect(testrun.request.calledOnce).be.ok();
+
+            var err = testrun.request.firstCall.args[0],
+                request = testrun.request.firstCall.args[3];
+
+            expect(err).to.be(null);
+            expect(request.url.toString()).to.eql('https://postman-echo.com/basic-auth');
+        });
     });
 });
