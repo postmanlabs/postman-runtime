382e5b5a5e08160b5d0f62161722dd507cff3518
kamalakannan <kamalakannan@getpostman.com>
2017-09-01 16:15:36 +0530

    Break fake auth test into smaller and cleaner specs

---
+++ test/integration/auth-methods/control-flow/typical.test.js
@@ -0,0 +1,151 @@
+var sinon = require('sinon'),
+    Authorizer = require('../../../../lib/authorizer/index').Authorizer;
+
+describe('auth control flow', function () {
+    var runOptions = {
+        collection: {
+            item: {
+                name: 'FakeAuth',
+                request: {
+                    url: 'https://postman-echo.com/basic-auth',
+                    auth: {
+                        type: 'fake',
+                        fake: {
+                            username: 'postman',
+                            password: 'password'
+                        }
+                    }
+                }
+            }
+        },
+        authorizer: {
+            interactive: true
+        }
+    };
+
+    describe('with working init, pre, and post', function () {
+        var testrun,
+            fakeHandler = {
+                init: function (auth, response, done) {
+                    done(null);
+                },
+                pre: function (auth, done) {
+                    done(null, true);
+                },
+                post: function (auth, response, done) {
+                    done(null, true);
+                },
+                sign: function (auth, request, done) {
+                    done();
+                }
+            },
+            handlerSpies = {
+                pre: sinon.spy(fakeHandler, 'pre'),
+                init: sinon.spy(fakeHandler, 'init'),
+                post: sinon.spy(fakeHandler, 'post'),
+                sign: sinon.spy(fakeHandler, 'sign')
+            };
+
+        before(function (done) {
+            Authorizer.addHandler(fakeHandler, 'fake');
+            // perform the collection run
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        after(function () {
+            Authorizer.removeHandler('fake');
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.callCount).to.be(1);
+            testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.callCount).to.be(1);
+            expect(testrun.io.callCount).to.be(1);
+        });
+
+        it('must have sent the request once', function () {
+            expect(testrun.request.callCount).to.be(1);
+
+            var err = testrun.request.firstCall.args[0],
+                request = testrun.request.firstCall.args[3];
+
+            expect(err).to.be(null);
+            expect(request.url.toString()).to.eql('https://postman-echo.com/basic-auth');
+        });
+
+        it('must call sign and post, not init', function () {
+            expect(handlerSpies.pre.callCount).to.be(1);
+            expect(handlerSpies.init.callCount).to.be(0);
+            expect(handlerSpies.post.callCount).to.be(1);
+            expect(handlerSpies.sign.callCount).to.be(1);
+        });
+    });
+
+    describe('with false result in pre', function () {
+        var testrun,
+            fakeHandler = {
+                init: function (auth, response, done) {
+                    done(null);
+                },
+                pre: function (auth, done) {
+                    done(null, false);
+                },
+                post: function (auth, response, done) {
+                    done(null, true);
+                },
+                sign: function (auth, request, done) {
+                    done();
+                }
+            },
+            handlerSpies = {
+                pre: sinon.spy(fakeHandler, 'pre'),
+                init: sinon.spy(fakeHandler, 'init'),
+                post: sinon.spy(fakeHandler, 'post'),
+                sign: sinon.spy(fakeHandler, 'sign')
+            };
+
+        before(function (done) {
+            Authorizer.addHandler(fakeHandler, 'fake');
+            // perform the collection run
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        after(function () {
+            Authorizer.removeHandler('fake');
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.callCount).to.be(1);
+            testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.callCount).to.be(1);
+            expect(testrun.io.callCount).to.be(1);
+        });
+
+        it('must have sent the request once', function () {
+            expect(testrun.request.callCount).to.be(1);
+
+            var err = testrun.request.firstCall.args[0],
+                request = testrun.request.firstCall.args[3];
+
+            expect(err).to.be(null);
+            expect(request.url.toString()).to.eql('https://postman-echo.com/basic-auth');
+        });
+
+        it('must skip signing', function () {
+            expect(handlerSpies.pre.callCount).to.be(1);
+            expect(handlerSpies.init.callCount).to.be(0);
+            expect(handlerSpies.sign.callCount).to.be(0);
+            expect(handlerSpies.post.callCount).to.be(1);
+        });
+    });
+});
