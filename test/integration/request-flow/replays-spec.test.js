c3207b7be4d894fbf27cc85165835afed2e13efe
kamalakannan <kamalakannan@getpostman.com>
2017-09-11 22:33:19 +0530

    Use `response` callback in test cases

--- test/integration/request-flow/replays-spec.test.js
+++ test/integration/request-flow/replays-spec.test.js
@@ -67,9 +67,10 @@ describe('requests replayed', function () {
         });
 
         it('must have sent the original request', function () {
-            var request = testrun.request.getCall(1).args[3],
-                response = testrun.request.getCall(1).args[2];
+            var request = testrun.response.getCall(0).args[3],
+                response = testrun.response.getCall(0).args[2];
 
+            expect(testrun.response.callCount).to.be(1);
             expect(request.url.toString()).to.eql('https://postman-echo.com/get');
             expect(response.code).to.eql(200);
         });
@@ -148,9 +149,8 @@ describe('requests replayed', function () {
             expect(testrun.start.callCount).to.be(1);
         });
 
-        // @todo: post helpers do not bubble errors yet
-        it.skip('must have ended with max count error', function () {
-            var err = testrun.request.lastCall.args[0];
+        it('must have ended with max count error', function () {
+            var err = testrun.response.lastCall.args[0];
 
             expect(err).to.have.property('message', 'runtime: maximum intermediate request limit exceeded');
         });
@@ -198,9 +198,8 @@ describe('requests replayed', function () {
             expect(testrun.start.callCount).to.be(1);
         });
 
-        // @todo: post helpers do not bubble errors yet
-        it.skip('must have ended with max count error', function () {
-            var err = testrun.request.lastCall.args[0];
+        it('must have ended with max count error', function () {
+            var err = testrun.response.lastCall.args[0];
 
             expect(err).to.have.property('message', 'runtime: maximum intermediate request limit exceeded');
         });
