a8e49eefd4b5450bf09a9056a81bebf2c5a8be7a
Harendra Singh <66hari@gmail.com>
2017-09-18 18:09:13 +0530

    Add NTLM fake server for test cases It is fake in the sense that the type2message is hard coded

--- test/integration/auth-methods/ntlm.test.js
+++ test/integration/auth-methods/ntlm.test.js
@@ -1,18 +1,24 @@
-var expect = require('expect.js');
+var expect = require('expect.js'),
+    http = require('http'),
+    ntlmUtil = require('httpntlm').ntlm;
 
 describe('NTLM', function () {
-    var testrun,
+    var ntlmServer,
+        ntlmServerPort = 3456,
+        testrun,
         runOptions = {
             collection: {
                 item: {
                     name: 'NTLM Sample Request',
                     request: {
-                        url: 'http://52.170.211.245',
+                        url: 'http://localhost:' + ntlmServerPort,
                         auth: {
                             type: 'ntlm',
                             ntlm: {
                                 username: '{{uname}}',
-                                password: '{{pass}}'
+                                password: '{{pass}}',
+                                domain: '{{domain}}',
+                                workstation: '{{workstation}}'
                             }
                         }
                     }
@@ -23,6 +29,61 @@ describe('NTLM', function () {
             }
         };
 
+    before(function () {
+        var username = 'postman',
+            password = 'ntlm',
+            domain = 'domain',
+            workstation = 'workstation',
+            type1message = ntlmUtil.createType1Message({domain: domain, workstation: workstation}),
+            // sample taken from a real IIS server
+            type2message = 'NTLM TlRMTVNTUAACAAAAHgAeADgAAAAFgoqi3ZUAgGmJcLAAAAAAAAAAAJgAmABWAAAABgOAJQAAAA93AGkAbgAt' +
+                'AHMAZQByAHYAZQByAC0AMgAwADEAMgACAB4AdwBpAG4ALQBzAGUAcgB2AGUAcgAtADIAMAAxADIAAQAeAHcAaQBuAC0AcwBlAHIA' +
+                'dgBlAHIALQAyADAAMQAyAAQAHgB3AGkAbgAtAHMAZQByAHYAZQByAC0AMgAwADEAMgADAB4AdwBpAG4ALQBzAGUAcgB2AGUAcgAt' +
+                'ADIAMAAxADIABwAIAD3f8WFvMNMBAAAAAA==',
+            type3message = ntlmUtil.createType3Message(ntlmUtil.parseType2Message(type2message),
+                {
+                    username: username,
+                    password: password,
+                    domain: domain,
+                    workstation: workstation
+                }
+            ),
+            headers = {
+                initialized: {'www-authenticate': 'NTLM'},
+                type2message: {'www-authenticate': type2message}
+            };
+
+        ntlmServer = new http.Server();
+        ntlmServer.on('request', function (req, res) {
+            var authHeader = req.headers.authorization,
+                header;
+
+            // Extended security is turned on, so only first 160 chars will match
+            if (authHeader && authHeader.slice(0, 160) === type3message.slice(0, 160)) {
+                res.writeHead(200);
+                return res.end('authorized');
+            }
+
+            if (authHeader === type1message) {
+                header = headers.type2message;
+            }
+            else {
+                header = headers.initialized;
+            }
+
+            res.writeHead(401, header);
+            res.end('unauthorized');
+        });
+
+        ntlmServer.listen(ntlmServerPort, function () {
+            console.log('NTLM server listening on port ' + ntlmServerPort);
+        });
+    });
+
+    after(function () {
+        ntlmServer.close();
+    });
+
     describe('with in-correct details', function () {
         before(function (done) {
             runOptions.environment = {
@@ -31,7 +92,13 @@ describe('NTLM', function () {
                     value: 'foo'
                 }, {
                     key: 'pass',
-                    value: 'bar'
+                    value: 'baz'
+                }, {
+                    key: 'domain',
+                    value: 'domain'
+                }, {
+                    key: 'workstation',
+                    value: 'workstation'
                 }]
             };
             // perform the collection run
@@ -65,10 +132,16 @@ describe('NTLM', function () {
             runOptions.environment = {
                 values: [{
                     key: 'uname',
-                    value: 'harryi3t'
+                    value: 'postman'
                 }, {
                     key: 'pass',
-                    value: '1234'
+                    value: 'password'
+                }, {
+                    key: 'domain',
+                    value: 'domain'
+                }, {
+                    key: 'workstation',
+                    value: 'workstation'
                 }]
             };
             // perform the collection run
@@ -78,7 +151,7 @@ describe('NTLM', function () {
             });
         });
 
-        it('must have completed the run', function () {
+        it('must have completed the run successfully', function () {
             expect(testrun).be.ok();
             expect(testrun.done.callCount).to.be(1);
             testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
