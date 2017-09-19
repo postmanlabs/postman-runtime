7740617fd7a2e0707541d5d7a10f05a2140d684c
Harendra Singh <66hari@gmail.com>
2017-09-19 19:08:56 +0530

    Add test case for in-correct endpoint mkaing it to bailout on first request

--- test/integration/auth-methods/ntlm.test.js
+++ test/integration/auth-methods/ntlm.test.js
@@ -1,4 +1,5 @@
-var expect = require('expect.js');
+var expect = require('expect.js'),
+    _ = require('lodash');
 
 describe('NTLM', function () {
     // @todo Add '/ntlm' endpoint in echo server
@@ -30,6 +31,50 @@ describe('NTLM', function () {
                 interactive: true
             }
         };
+
+    describe('with in-correct endpoint', function () {
+        before(function (done) {
+            var clonedRunOptions = _.cloneDeep(runOptions);
+
+            clonedRunOptions.environment = {
+                values: [{
+                    key: 'uname',
+                    value: USERNAME
+                }, {
+                    key: 'pass',
+                    value: PASSWORD
+                }, {
+                    key: 'domain',
+                    value: DOMAIN
+                }, {
+                    key: 'workstation',
+                    value: WORKSTATION
+                }]
+            };
+            clonedRunOptions.collection.item.request.url = 'http://postman-echo.com/get';
+            // perform the collection run
+            this.run(clonedRunOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.callCount).to.be(1);
+            testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.callCount).to.be(1);
+        });
+
+        it('must bail out after sending one request', function () {
+            expect(testrun.request.callCount).to.be(1);
+
+            var err = testrun.request.firstCall.args[0];
+
+            expect(err).to.be(null);
+        });
+    });
 
     describe('with in-correct details', function () {
         before(function (done) {
