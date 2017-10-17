94a8120adaaa729fb1daace9a7016d530a0d1e88
Harendra Singh <66hari@gmail.com>
2017-10-17 14:39:50 +0530

    Adds test case for request level auth overriding collection level auth

--- test/integration/inherited-entities/auth.test.js
+++ test/integration/inherited-entities/auth.test.js
@@ -16,7 +16,7 @@ describe('Inherited Auth', function () {
             }
         };
 
-    describe('request level', function () {
+    describe('in request level', function () {
         before(function (done) {
             var clonedRunOptions = _.merge({}, runOptions,
                 {
@@ -73,7 +73,7 @@ describe('Inherited Auth', function () {
         });
     });
 
-    describe('itemGroup level', function () {
+    describe('in itemGroup level', function () {
         before(function (done) {
             var clonedRunOptions = _.merge({}, runOptions,
                 {
@@ -135,7 +135,7 @@ describe('Inherited Auth', function () {
         });
     });
 
-    describe('collection level', function () {
+    describe('in collection level', function () {
         before(function (done) {
             var clonedRunOptions = _.merge({}, runOptions,
                 {
@@ -145,6 +145,70 @@ describe('Inherited Auth', function () {
                             basic: {
                                 username: '{{uname}}',
                                 password: '{{pass}}'
+                            }
+                        }
+                    },
+                    environment: {
+                        values: [{
+                            key: 'uname',
+                            value: 'postman'
+                        }, {
+                            key: 'pass',
+                            value: 'password'
+                        }]
+                    }
+                }
+            );
+
+            // perform the collection run
+            this.run(clonedRunOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.callCount).to.be(1);
+            testrun.done.getCall(0).args[0] && console.error(testrun.done.getCall(0).args[0].stack);
+            expect(testrun.done.getCall(0).args[0]).to.be(null);
+            expect(testrun.start.callCount).to.be(1);
+        });
+
+        it('must have sent the request once', function () {
+            expect(testrun.request.callCount).to.be(1);
+
+            var err = testrun.request.firstCall.args[0],
+                request = testrun.request.firstCall.args[3],
+                response = testrun.request.firstCall.args[2];
+
+            expect(err).to.be(null);
+            expect(request.url.toString()).to.eql('https://postman-echo.com/basic-auth');
+            expect(response.code).to.eql(200);
+        });
+    });
+
+    describe('in collection and request level', function () {
+        before(function (done) {
+            var clonedRunOptions = _.merge({}, runOptions,
+                {
+                    collection: {
+                        item: {
+                            request: {
+                                auth: {
+                                    type: 'basic',
+                                    basic: {
+                                        username: '{{uname}}',
+                                        password: '{{pass}}'
+                                    }
+                                }
+                            }
+                        },
+                        auth: {
+                            type: 'basic',
+                            basic: {
+                                username: 'iamnotpostman',
+                                password: 'password'
                             }
                         }
                     },
