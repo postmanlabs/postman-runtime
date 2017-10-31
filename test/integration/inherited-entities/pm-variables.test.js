b88886a48c29c5e22177dc414e267493f20033bc
Harendra Singh <66hari@gmail.com>
2017-10-31 15:50:27 +0530

    Add _variables to state so that can be shared across requests

--- test/integration/inherited-entities/pm-variables.test.js
+++ test/integration/inherited-entities/pm-variables.test.js
@@ -1,17 +1,15 @@
-describe('pm.variables', function () {
-    var testRun;
+var _ = require('lodash');
 
-    before(function (done) {
-        var runOptions = {
+describe('pm.variables', function () {
+    var testRun,
+        runOptions = {
             data: [{
                 'key-4': 'data-value-4'
             }],
             globals: {
                 values: [
                     {key: 'key-1', value: 'global-value-1', name: 'key-1', enabled: true},
-                    {key: 'key-2', value: 'global-value-2', name: 'key-2', enabled: true},
-                    {key: 'key-3', value: 'global-value-3', name: 'key-3', enabled: true},
-                    {key: 'key-4', value: 'global-value-4', name: 'key-4', enabled: true}
+                    {key: 'key-2', value: 'global-value-2', name: 'key-2', enabled: true}
                 ]
             },
             environment: {
@@ -23,35 +21,123 @@ describe('pm.variables', function () {
             collection: {
                 variable: [
                     {key: 'key-2', value: 'coll-value-2', name: 'key-2', enabled: true},
-                    {key: 'key-3', value: 'coll-value-3', name: 'key-3', enabled: true},
-                    {key: 'key-4', value: 'coll-value-4', name: 'key-4', enabled: true}
+                    {key: 'key-3', value: 'coll-value-3', name: 'key-3', enabled: true}
+                ],
+                event: [
+                    {
+                        listen: 'prerequest',
+                        script: {
+                            exec: 'console.log(pm.variables.toObject())'
+                        }
+                    },
+                    {
+                        listen: 'test',
+                        script: {
+                            exec: 'console.log(pm.variables.toObject())'
+                        }
+                    }
                 ],
                 item: {
+                    name: 'Sample Request 1',
+                    event: [
+                        {
+                            listen: 'prerequest',
+                            script: {
+                                exec: 'console.log(pm.variables.toObject())'
+                            }
+                        },
+                        {
+                            listen: 'test',
+                            script: {
+                                exec: 'console.log(pm.variables.toObject())'
+                            }
+                        }
+                    ],
+                    request: {
+                        url: 'https://postman-echo.com/get?param={{key-1}}:{{key-2}}:{{key-3}}:{{key-4}}',
+                        auth: {
+                            type: 'bearer',
+                            bearer: {
+                                token: '{{key-1}}:{{key-2}}:{{key-3}}:{{key-4}}'
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+    describe('precedence', function () {
+        before(function (done) {
+            this.run(runOptions, function (err, results) {
+                testRun = results;
+
+                done(err);
+            });
+        });
+
+        it('must be honoured in scripts', function () {
+            var consoleLogs = testRun.console.getCall(0).args.slice(2)
+                .concat(testRun.console.getCall(1).args.slice(2))
+                .concat(testRun.console.getCall(2).args.slice(2))
+                .concat(testRun.console.getCall(3).args.slice(2));
+
+            consoleLogs.forEach(function (consoleLog) {
+                expect(consoleLog).to.eql({
+                    'key-1': 'global-value-1',
+                    'key-2': 'coll-value-2',
+                    'key-3': 'env-value-3',
+                    'key-4': 'data-value-4'
+                });
+            });
+        });
+
+        it('must be honoured in request URL', function () {
+            var url = testRun.request.getCall(0).args[3].url.toString(),
+                expectedParam = 'global-value-1:coll-value-2:env-value-3:data-value-4';
+
+            expect(url).to.be('https://postman-echo.com/get?param=' + expectedParam);
+        });
+
+        it('must be honoured in auth', function () {
+            var response = testRun.response.getCall(0).args[2],
+                expectedToken = 'global-value-1:coll-value-2:env-value-3:data-value-4';
+
+            expect(response.json().headers).to.have.property('authorization', 'Bearer ' + expectedToken);
+        });
+    });
+
+    describe('getters and setters', function () {
+        before(function (done) {
+            var clonedRunOptions = _.merge({}, runOptions, {
+                collection: {
                     event: [
                         {
                             listen: 'prerequest',
                             script: {
                                 exec: `
-                                console.log('collection pre before', pm.variables.toObject())
-                                pm.variables.set('key-1', 'modified-1');
-                                pm.variables.set('key-2', 'modified-1');
-                                pm.variables.set('key-3', 'modified-1');
-                                pm.variables.set('key-4', 'modified-1');
-                                console.log('collection pre after', pm.variables.toObject())
+                                // only run for 1st item
+                                if (pm.variables.get('key-1') !== 'modified-1') {
+                                    pm.variables.set('key-1', 'modified-1');
+                                    pm.variables.set('key-2', 'modified-1');
+                                    console.log('collection pre', pm.variables.toObject())
+                                }
                             `}
                         },
                         {
                             listen: 'test',
                             script: {
                                 exec: `
-                                pm.variables.set('key-3', 'modified-3');
-                                pm.variables.set('key-4', 'modified-3');
-                                console.log('collection test after', pm.variables.toObject())
+                                // only run for 1st item
+                                if (pm.variables.get('key-3') !== 'modified-3') {
+                                    pm.variables.set('key-3', 'modified-3');
+                                    pm.variables.set('key-4', 'modified-3');
+                                    console.log('collection test', pm.variables.toObject())
+                                }
                             `}
                         }
                     ],
                     item: [{
-                        name: 'Test pm.variables persistence across scripts',
+                        name: 'Sample Request 1',
                         event: [
                             {
                                 listen: 'prerequest',
@@ -59,8 +145,7 @@ describe('pm.variables', function () {
                                     exec: `
                                     pm.variables.set('key-2', 'modified-2');
                                     pm.variables.set('key-3', 'modified-2');
-                                    pm.variables.set('key-4', 'modified-2');
-                                    console.log('item 1 pre after', pm.variables.toObject())
+                                    console.log('item 1 pre', pm.variables.toObject())
                                 `}
                             },
                             {
@@ -68,7 +153,7 @@ describe('pm.variables', function () {
                                 script: {
                                     exec: `
                                     pm.variables.set('key-4', 'modified-4');
-                                    console.log('item 1 test after', pm.variables.toObject())
+                                    console.log('item 1 test', pm.variables.toObject())
                                 `}
                             }
                         ],
@@ -82,7 +167,7 @@ describe('pm.variables', function () {
                             }
                         }
                     }, {
-                        name: 'Test pm.variables persistence across requests',
+                        name: 'Sample Request 2',
                         event: [
                             {
                                 listen: 'prerequest',
@@ -99,58 +184,50 @@ describe('pm.variables', function () {
                                 `}
                             }
                         ],
-                        request: 'https://postman-echo.com/get'
+                        request: {
+                            url: 'https://postman-echo.com/get?param={{key-1}}:{{key-2}}:{{key-3}}:{{key-4}}',
+                            auth: {
+                                type: 'bearer',
+                                bearer: {
+                                    token: '{{key-1}}:{{key-2}}:{{key-3}}:{{key-4}}'
+                                }
+                            }
+                        }
                     }]
                 }
-            }
-        };
+            });
+            this.run(clonedRunOptions, function (err, results) {
+                testRun = results;
 
-        // perform the collection run
-        this.run(runOptions, function (err, results) {
-            testRun = results;
-
-            done(err);
+                done(err);
+            });
         });
-    });
 
-    it('must have completed the run', function () {
-        expect(testRun).be.ok();
-        expect(testRun.done.callCount).to.be(1);
-        testRun.done.getCall(0).args[0] && console.error(testRun.done.getCall(0).args[0].stack);
-        expect(testRun.done.getCall(0).args[0]).to.be(null);
-        expect(testRun.start.callCount).to.be(1);
-    });
-
-    it('must have executed the events correctly', function () {
-        expect(testRun.prerequest.callCount).to.be(2);
-        expect(testRun.test.callCount).to.be(2);
-        expect(testRun.console.callCount).to.be(10);
-    });
-
-    describe('.set', function () {
         it('must persist any changes accross scripts', function () {
-            var collPreConsoleAfter = testRun.console.getCall(1).args.slice(2),
-                itemPreConsole = testRun.console.getCall(2).args.slice(2),
-                collTestConsole = testRun.console.getCall(3).args.slice(2),
-                itemTestConsole = testRun.console.getCall(4).args.slice(2);
+            var collPreConsole = testRun.console.getCall(0).args.slice(2),
+                itemPreConsole = testRun.console.getCall(1).args.slice(2),
+                collTestConsole = testRun.console.getCall(2).args.slice(2),
+                itemTestConsole = testRun.console.getCall(3).args.slice(2),
+                item2PreConsole = testRun.console.getCall(4).args.slice(2),
+                item2TestConsole = testRun.console.getCall(5).args.slice(2);
 
-            expect(collPreConsoleAfter[0]).to.be('collection pre after');
-            expect(collPreConsoleAfter[1]).to.eql({
+            expect(collPreConsole[0]).to.be('collection pre');
+            expect(collPreConsole[1]).to.eql({
                 'key-1': 'modified-1',
                 'key-2': 'modified-1',
-                'key-3': 'modified-1',
-                'key-4': 'modified-1'
+                'key-3': 'env-value-3',
+                'key-4': 'data-value-4'
             });
 
-            expect(itemPreConsole[0]).to.be('item 1 pre after');
+            expect(itemPreConsole[0]).to.be('item 1 pre');
             expect(itemPreConsole[1]).to.eql({
                 'key-1': 'modified-1',
                 'key-2': 'modified-2',
                 'key-3': 'modified-2',
-                'key-4': 'modified-2'
+                'key-4': 'data-value-4'
             });
 
-            expect(collTestConsole[0]).to.be('collection test after');
+            expect(collTestConsole[0]).to.be('collection test');
             expect(collTestConsole[1]).to.eql({
                 'key-1': 'modified-1',
                 'key-2': 'modified-2',
@@ -158,24 +235,28 @@ describe('pm.variables', function () {
                 'key-4': 'modified-3'
             });
 
-            expect(itemTestConsole[0]).to.be('item 1 test after');
+            expect(itemTestConsole[0]).to.be('item 1 test');
             expect(itemTestConsole[1]).to.eql({
                 'key-1': 'modified-1',
                 'key-2': 'modified-2',
                 'key-3': 'modified-3',
                 'key-4': 'modified-4'
             });
-        });
 
-        it('must not persist any changes accross requests', function () {
-            var collPreConsoleBefore = testRun.console.getCall(5).args.slice(2);
+            expect(item2PreConsole[0]).to.be('item 2 pre');
+            expect(item2PreConsole[1]).to.eql({
+                'key-1': 'modified-1',
+                'key-2': 'modified-2',
+                'key-3': 'modified-3',
+                'key-4': 'modified-4'
+            });
 
-            expect(collPreConsoleBefore[0]).to.be('collection pre before');
-            expect(collPreConsoleBefore[1]).to.eql({
-                'key-1': 'global-value-1',
-                'key-2': 'coll-value-2',
-                'key-3': 'env-value-3',
-                'key-4': 'data-value-4'
+            expect(item2TestConsole[0]).to.be('item 2 test');
+            expect(item2TestConsole[1]).to.eql({
+                'key-1': 'modified-1',
+                'key-2': 'modified-2',
+                'key-3': 'modified-3',
+                'key-4': 'modified-4'
             });
         });
 
@@ -194,44 +275,33 @@ describe('pm.variables', function () {
                 });
                 expect(event.result.collectionVariables.toObject()).to.eql({
                     'key-2': 'coll-value-2',
-                    'key-3': 'coll-value-3',
-                    'key-4': 'coll-value-4'
+                    'key-3': 'coll-value-3'
                 });
                 expect(event.result.globals.toObject()).to.eql({
                     'key-1': 'global-value-1',
-                    'key-2': 'global-value-2',
-                    'key-3': 'global-value-3',
-                    'key-4': 'global-value-4'
+                    'key-2': 'global-value-2'
                 });
-            });
-        });
-    });
-
-    describe('.get', function () {
-        it('must honour the variable precedence', function () {
-            var collPreConsoleBefore = testRun.console.getCall(0).args.slice(2);
-
-            expect(collPreConsoleBefore[0]).to.be('collection pre before');
-            expect(collPreConsoleBefore[1]).to.eql({
-                'key-1': 'global-value-1',
-                'key-2': 'coll-value-2',
-                'key-3': 'env-value-3',
-                'key-4': 'data-value-4'
             });
         });
 
         it('must be resolved in request URL', function() {
-            var url = testRun.request.getCall(0).args[3].url.toString(),
-                expectedParam = 'global-value-1:coll-value-2:env-value-3:data-value-4';
+            var url1 = testRun.request.getCall(0).args[3].url.toString(),
+                url2 = testRun.request.getCall(1).args[3].url.toString(),
+                expectedToken1 = 'modified-1:modified-2:modified-2:data-value-4',
+                expectedToken2 = 'modified-1:modified-2:modified-3:modified-4';
 
-            expect(url).to.be('https://postman-echo.com/get?param=' + expectedParam);
+            expect(url1).to.be('https://postman-echo.com/get?param=' + expectedToken1);
+            expect(url2).to.be('https://postman-echo.com/get?param=' + expectedToken2);
         });
 
         it('must be resolved in request auth', function() {
-            var response = testRun.response.getCall(0).args[2],
-                expectedToken = 'global-value-1:coll-value-2:env-value-3:data-value-4';
+            var response1 = testRun.response.getCall(0).args[2],
+                response2 = testRun.response.getCall(1).args[2],
+                expectedToken1 = 'modified-1:modified-2:modified-2:data-value-4',
+                expectedToken2 = 'modified-1:modified-2:modified-3:modified-4';
 
-            expect(response.json().headers).to.have.property('authorization', 'Bearer ' + expectedToken);
+            expect(response1.json().headers).to.have.property('authorization', 'Bearer ' + expectedToken1);
+            expect(response2.json().headers).to.have.property('authorization', 'Bearer ' + expectedToken2);
         });
     });
 });
