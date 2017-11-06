643e4d600419eade7438ff991f886df91eb24a94
kamalakannan <kamalakannan@getpostman.com>
2017-11-06 22:05:38 +0530

    Sanitize timeout options to treat to 0 as Infinity

--- test/integration/sanity/timeouts/sync-scripts.test.js
+++ test/integration/sanity/timeouts/sync-scripts.test.js
@@ -45,6 +45,52 @@ describe('synchronous script timeouts', function () {
         });
     });
 
+    describe('not breached with infinite timeout', function () {
+        var testrun;
+
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        event: [{
+                            listen: 'prerequest',
+                            script: `
+                            var now = Date.now(),
+                                later = now + 300;
+                            while(Date.now() < later);
+                        `
+                        }],
+                        request: {
+                            url: 'https://postman-echo.com/get',
+                            method: 'GET'
+                        }
+                    }]
+                },
+                timeout: {
+                    script: 0
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('must have completed the run', function () {
+            expect(testrun).be.ok();
+            expect(testrun.done.callCount).to.be(1);
+            expect(testrun.done.firstCall.args[0]).to.not.be.ok();
+            expect(testrun.start.callCount).to.be(1);
+        });
+
+        it('must handle script timeouts correctly', function () {
+            expect(testrun).to.be.ok();
+            expect(testrun.prerequest.callCount).to.be(1);
+
+            expect(testrun.prerequest.firstCall.args[0]).to.be(null);
+            expect(testrun.prerequest.firstCall.args[2][0]).to.not.have.property('error');
+        });
+    });
+
     describe('breached', function () {
         describe('global timeout', function () {
             var testrun,
