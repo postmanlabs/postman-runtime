a641d70414b0441be5cfe0399532da9d911664f6
Udit Vasu <udit.vasu@getpostman.com>
2018-09-20 23:01:26 +0530

    Update tests for protocolProfileBehavior

--- test/integration/request-body/protocolProfileBehavior.test.js
+++ test/integration/request-body/protocolProfileBehavior.test.js
@@ -26,137 +26,143 @@ describe('protocolProfileBehavior', function () {
     });
 
     describe('with disableBodyPruning: true', function () {
-        before(function (done) {
-            this.run({
-                collection: {
-                    item: [{
-                        request: {
-                            url: URL,
-                            method: 'GET',
-                            body: {
-                                mode: 'raw',
-                                raw: 'foo=bar'
+        describe('HTTP GET', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'GET',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
+                            },
+                            protocolProfileBehavior: {
+                                disableBodyPruning: true
                             }
-                        },
-                        protocolProfileBehavior: {
-                            disableBodyPruning: true
-                        }
-                    }]
-                }
-            }, function (err, results) {
-                testrun = results;
-                done(err);
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
             });
-        });
 
-        it('should complete the run', function () {
-            expect(testrun).be.ok();
-            sinon.assert.calledOnce(testrun.start);
-            sinon.assert.calledOnce(testrun.done);
-            sinon.assert.calledWith(testrun.done.getCall(0), null);
-        });
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
 
-        it('should send body with GET method correctly', function () {
-            sinon.assert.calledOnce(testrun.request);
-            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            it('should send body with GET method correctly', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
 
-            sinon.assert.calledOnce(testrun.response);
-            sinon.assert.calledWith(testrun.response.getCall(0), null);
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
 
-            var response = testrun.request.getCall(0).args[2].stream.toString();
+                var response = testrun.request.getCall(0).args[2].stream.toString();
 
-            expect(response).to.contain('GET / HTTP/1.1');
-            expect(response).to.contain('Content-Type: text/plain');
-            expect(response).to.contain('foo=bar');
+                expect(response).to.contain('GET / HTTP/1.1');
+                expect(response).to.contain('Content-Type: text/plain');
+                expect(response).to.contain('foo=bar');
+            });
         });
     });
 
     describe('with disableBodyPruning: false', function () {
-        before(function (done) {
-            this.run({
-                collection: {
-                    item: [{
-                        request: {
-                            url: URL,
-                            method: 'GET',
-                            body: {
-                                mode: 'raw',
-                                raw: 'foo=bar'
+        describe('HTTP GET', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'GET',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
+                            },
+                            protocolProfileBehavior: {
+                                disableBodyPruning: false
                             }
-                        },
-                        protocolProfileBehavior: {
-                            disableBodyPruning: false
-                        }
-                    }]
-                }
-            }, function (err, results) {
-                testrun = results;
-                done(err);
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
             });
-        });
 
-        it('should complete the run', function () {
-            expect(testrun).be.ok();
-            sinon.assert.calledOnce(testrun.start);
-            sinon.assert.calledOnce(testrun.done);
-            sinon.assert.calledWith(testrun.done.getCall(0), null);
-        });
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
 
-        it('should not send body with GET method', function () {
-            sinon.assert.calledOnce(testrun.request);
-            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            it('should not send body with GET method', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
 
-            sinon.assert.calledOnce(testrun.response);
-            sinon.assert.calledWith(testrun.response.getCall(0), null);
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
 
-            var response = testrun.request.getCall(0).args[2].stream.toString();
+                var response = testrun.request.getCall(0).args[2].stream.toString();
 
-            expect(response).to.contain('GET / HTTP/1.1');
-            expect(response).to.not.contain('Content-Type');
-            expect(response).to.not.contain('foo=bar');
+                expect(response).to.contain('GET / HTTP/1.1');
+                expect(response).to.not.contain('Content-Type');
+                expect(response).to.not.contain('foo=bar');
+            });
         });
     });
 
     describe('with disableBodyPruning: undefined', function () {
-        before(function (done) {
-            this.run({
-                collection: {
-                    item: [{
-                        request: {
-                            url: URL,
-                            method: 'GET',
-                            body: {
-                                mode: 'raw',
-                                raw: 'foo=bar'
+        describe('HTTP GET', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'GET',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
                             }
-                        }
-                    }]
-                }
-            }, function (err, results) {
-                testrun = results;
-                done(err);
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
             });
-        });
 
-        it('should complete the run', function () {
-            expect(testrun).be.ok();
-            sinon.assert.calledOnce(testrun.start);
-            sinon.assert.calledOnce(testrun.done);
-            sinon.assert.calledWith(testrun.done.getCall(0), null);
-        });
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
 
-        it('should not send body by default for GET method', function () {
-            sinon.assert.calledOnce(testrun.request);
-            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            it('should not send body by default for GET method', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
 
-            sinon.assert.calledOnce(testrun.response);
-            sinon.assert.calledWith(testrun.response.getCall(0), null);
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
 
-            var response = testrun.request.getCall(0).args[2].stream.toString();
+                var response = testrun.request.getCall(0).args[2].stream.toString();
 
-            expect(response).to.contain('GET / HTTP/1.1');
-            expect(response).to.not.contain('Content-Type');
-            expect(response).to.not.contain('foo=bar');
+                expect(response).to.contain('GET / HTTP/1.1');
+                expect(response).to.not.contain('Content-Type');
+                expect(response).to.not.contain('foo=bar');
+            });
         });
     });
 });
