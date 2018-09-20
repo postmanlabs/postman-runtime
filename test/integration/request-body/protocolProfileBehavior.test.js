439b2ebfe635d7513ea355d3d37efc9b8222ae58
Udit Vasu <udit.vasu@getpostman.com>
2018-09-21 00:02:13 +0530

    Add tests for  protocolProfileBehavior using HEAD request method

--- test/integration/request-body/protocolProfileBehavior.test.js
+++ test/integration/request-body/protocolProfileBehavior.test.js
@@ -4,6 +4,7 @@ var net = require('net'),
 describe('protocolProfileBehavior', function () {
     var server,
         testrun,
+        rawRequest,
         PORT = 5050,
         URL = 'http://localhost:' + PORT;
 
@@ -15,8 +16,20 @@ describe('protocolProfileBehavior', function () {
                 var raw = chunk.toString(); // Request Message: [POSTMAN / HTTP/1.1 ...]
                 socket.write('HTTP/1.1 200 ok\r\n');
                 socket.write('Content-Type: text/plain\r\n\r\n');
-                socket.write(raw); // respond with raw request message
+
+                // Respond with raw request message.
+                //
+                // @note http-parser will blow up if body is sent for HEAD request.
+                // RFC-7231: The HEAD method is identical to GET except that the
+                //           server MUST NOT send a message body in the response.
+                if (!raw.startsWith('HEAD / HTTP/1.1')) {
+                    socket.write(raw);
+                }
+
                 socket.end();
+
+                // store raw request message for testing HEAD requests.
+                rawRequest = raw;
             });
         }).listen(PORT, done);
     });
@@ -68,6 +81,54 @@ describe('protocolProfileBehavior', function () {
 
                 expect(response).to.contain('GET / HTTP/1.1');
                 expect(response).to.contain('Content-Type: text/plain');
+                expect(response).to.contain('content-length: 7');
+                expect(response).to.contain('foo=bar');
+            });
+        });
+
+        describe('HTTP HEAD', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'HEAD',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
+                            },
+                            protocolProfileBehavior: {
+                                disableBodyPruning: true
+                            }
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
+            });
+
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
+
+            it('should send body with HEAD method correctly', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
+
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
+
+                var response = rawRequest; // raw request message for this request
+
+                expect(response).to.contain('HEAD / HTTP/1.1');
+                expect(response).to.contain('Content-Type: text/plain');
+                expect(response).to.contain('content-length: 7');
                 expect(response).to.contain('foo=bar');
             });
         });
@@ -119,6 +180,52 @@ describe('protocolProfileBehavior', function () {
                 expect(response).to.not.contain('foo=bar');
             });
         });
+
+        describe('HTTP HEAD', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'HEAD',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
+                            },
+                            protocolProfileBehavior: {
+                                disableBodyPruning: false
+                            }
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
+            });
+
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
+
+            it('should not send body with HEAD method', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
+
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
+
+                var response = rawRequest; // raw request message for this request
+
+                expect(response).to.contain('HEAD / HTTP/1.1');
+                expect(response).to.not.contain('Content-Type');
+                expect(response).to.not.contain('foo=bar');
+            });
+        });
     });
 
     describe('with disableBodyPruning: undefined', function () {
@@ -160,6 +267,49 @@ describe('protocolProfileBehavior', function () {
                 var response = testrun.request.getCall(0).args[2].stream.toString();
 
                 expect(response).to.contain('GET / HTTP/1.1');
+                expect(response).to.not.contain('Content-Type');
+                expect(response).to.not.contain('foo=bar');
+            });
+        });
+
+        describe('HTTP HEAD', function () {
+            before(function (done) {
+                this.run({
+                    collection: {
+                        item: [{
+                            request: {
+                                url: URL,
+                                method: 'HEAD',
+                                body: {
+                                    mode: 'raw',
+                                    raw: 'foo=bar'
+                                }
+                            }
+                        }]
+                    }
+                }, function (err, results) {
+                    testrun = results;
+                    done(err);
+                });
+            });
+
+            it('should complete the run', function () {
+                expect(testrun).be.ok();
+                sinon.assert.calledOnce(testrun.start);
+                sinon.assert.calledOnce(testrun.done);
+                sinon.assert.calledWith(testrun.done.getCall(0), null);
+            });
+
+            it('should not send body by default for HEAD method', function () {
+                sinon.assert.calledOnce(testrun.request);
+                sinon.assert.calledWith(testrun.request.getCall(0), null);
+
+                sinon.assert.calledOnce(testrun.response);
+                sinon.assert.calledWith(testrun.response.getCall(0), null);
+
+                var response = rawRequest; // raw request message for this request
+
+                expect(response).to.contain('HEAD / HTTP/1.1');
                 expect(response).to.not.contain('Content-Type');
                 expect(response).to.not.contain('foo=bar');
             });
