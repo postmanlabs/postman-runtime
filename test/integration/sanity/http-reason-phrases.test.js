44ead39e8a4f4dfa28cdc6fdf9fa5488d7b7e99d
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-18 15:06:50 +0530

    Update integration/sanity/http-reason-phrases tests to chai

--- test/integration/sanity/http-reason-phrases.test.js
+++ test/integration/sanity/http-reason-phrases.test.js
@@ -1,3 +1,5 @@
+var expect = require('chai').expect;
+
 describe('http reasons', function () {
     var http = require('http'),
 
@@ -31,20 +33,26 @@ describe('http reasons', function () {
         server.listen(0, 'localhost');
     });
 
-    it('must have started and completed the test run', function () {
-        expect(testrun).be.ok();
-        expect(testrun.done.calledOnce).be.ok();
-        expect(testrun.start.calledOnce).be.ok();
+    it('should have started and completed the test run', function () {
+        expect(testrun).to.be.ok;
+        expect(testrun).to.nested.include({
+            'done.calledOnce': true,
+            'start.calledOnce': true
+        });
     });
 
-    it('must receive the correct reason phrase from the server', function () {
+    it('should receive the correct reason phrase from the server', function () {
         var response = testrun.request.getCall(0).args[2];
 
-        expect(response.code).be(400);
-        expect(response.status).be('Some Custom Reason');
-        expect(response.details()).to.have.property('code', 400);
-        expect(response.details()).to.have.property('name', 'Some Custom Reason');
-        expect(response.details()).to.have.property('detail', 'The request cannot be fulfilled due to bad syntax.');
+        expect(response).to.deep.include({
+            code: 400,
+            status: 'Some Custom Reason'
+        });
+        expect(response.details()).to.deep.include({
+            code: 400,
+            name: 'Some Custom Reason',
+            detail: 'The request cannot be fulfilled due to bad syntax.'
+        });
     });
 
     after(function () {
