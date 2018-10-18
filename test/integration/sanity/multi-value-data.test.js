8641a024330c839a20530f80652b5b4449405f59
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-18 15:08:01 +0530

    Update integration/sanity/multi-value-data tests to chai

--- test/integration/sanity/multi-value-data.test.js
+++ test/integration/sanity/multi-value-data.test.js
@@ -1,3 +1,5 @@
+var expect = require('chai').expect;
+
 describe('Multi value data', function() {
     var testrun;
 
@@ -28,20 +30,26 @@ describe('Multi value data', function() {
         });
     });
 
-    it('must have run the test script successfully', function() {
+    it('should have run the test script successfully', function() {
         var assertions = testrun.assertion.getCall(0).args[1];
-        expect(testrun).be.ok();
-        expect(testrun.test.calledOnce).be.ok();
+        expect(testrun).to.be.ok;
+        expect(testrun).to.nested.include({
+            'test.calledOnce': true
+        });
 
-        expect(testrun.test.getCall(0).args[0]).to.be(null);
-        expect(assertions[0]).to.have.property('name', 'working');
-        expect(assertions[0]).to.have.property('passed', true);
+        expect(testrun.test.getCall(0).args[0]).to.be.null;
+        expect(assertions[0]).to.nested.include({
+            name: 'working',
+            passed: true
+        });
     });
 
-    it('must have completed the run', function() {
-        expect(testrun).be.ok();
-        expect(testrun.done.calledOnce).be.ok();
-        expect(testrun.done.getCall(0).args[0]).to.be(null);
-        expect(testrun.start.calledOnce).be.ok();
+    it('should have completed the run', function() {
+        expect(testrun).to.be.ok;
+        expect(testrun.done.getCall(0).args[0]).to.be.null;
+        expect(testrun).to.nested.include({
+            'done.calledOnce': true,
+            'start.calledOnce': true
+        });
     });
 });
