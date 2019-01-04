273d8de0490f8435d778018f27275d087a9fdb7c
Udit Vasu <udit.vasu@getpostman.com>
2019-01-04 15:51:33 +0530

    Add integration tests for request headers

---
+++ test/integration/request-flow/headers.test.js
@@ -0,0 +1,172 @@
+var http = require('http'),
+    sinon = require('sinon'),
+    expect = require('chai').expect,
+    enableServerDestroy = require('server-destroy');
+
+describe('request headers', function () {
+    var server,
+        testrun,
+        PORT = 5050,
+        HOST = 'http://localhost:' + PORT;
+
+    /**
+     * Converts raw request headers to array of key-value object.
+     *
+     * [ 'User-Agent', 'PostmanRuntime' ] -> [{key: 'User-Agent', value: 'PostmanRuntime'}]
+     *
+     * @param {String[]} rawHeaders - raw request headers
+     * @returns {Object[]}
+     */
+    function parseRawHeaders(rawHeaders) {
+        if (!Array.isArray(rawHeaders)) { return []; }
+
+        var headerList = [],
+            i,
+            ii;
+
+        for (i = 0, ii = rawHeaders.length; i < ii; i += 2) {
+            headerList.push({
+                key: rawHeaders[i],
+                value: rawHeaders[i + 1]
+            });
+        }
+
+        return headerList;
+    }
+
+    before(function (done) {
+        server = http.createServer(function (req, res) {
+            res.writeHead(200, {'content-type': 'application/json'});
+            res.end(JSON.stringify(parseRawHeaders(req.rawHeaders)));
+        }).listen(PORT, function (err) {
+            if (err) { return done(err); }
+
+            this.run({
+                collection: {
+                    item: [{
+                        name: 'Duplicate headers',
+                        request: {
+                            url: HOST,
+                            header: [{
+                                key: 'Header-Name',
+                                value: 'value1'
+                            }, {
+                                key: 'Header-Name',
+                                value: 'value2'
+                            }]
+                        }
+                    }, {
+                        name: 'Disabled & Falsy headers',
+                        request: {
+                            url: HOST,
+                            header: [{
+                                key: 'Header-Name-1',
+                                value: 'value1'
+                            }, {
+                                key: 'Header-Name-2',
+                                value: 'value2',
+                                disabled: true
+                            }, {
+                                key: '',
+                                value: 'value3'
+                            }]
+                        }
+                    }, {
+                        name: 'Case Insensitive',
+                        request: {
+                            url: HOST,
+                            header: [{
+                                key: 'Header-Name-0',
+                                value: 'value0'
+                            }, {
+                                key: 'Header-Name-1',
+                                value: 'value1'
+                            }, {
+                                key: 'header-name-1',
+                                value: 'value2'
+                            }, {
+                                key: 'HEADER-NAME-2',
+                                value: 'value3'
+                            }]
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        }.bind(this));
+        enableServerDestroy(server);
+    });
+
+    after(function (done) {
+        server.destroy(done);
+    });
+
+    it('should complete the run', function () {
+        expect(testrun).to.be.ok;
+        sinon.assert.calledOnce(testrun.start);
+        sinon.assert.calledOnce(testrun.done);
+        sinon.assert.calledWith(testrun.done.getCall(0), null);
+
+        sinon.assert.calledThrice(testrun.request);
+        sinon.assert.calledThrice(testrun.response);
+    });
+
+    it('should handle duplicate headers correctly', function () {
+        sinon.assert.calledWith(testrun.request.getCall(0), null);
+        sinon.assert.calledWith(testrun.response.getCall(0), null);
+
+        var response = testrun.response.getCall(0).args[2],
+            requestHeaders = JSON.parse(response.stream);
+
+        expect(requestHeaders).to.deep.include.members([
+            {key: 'Header-Name', value: 'value1'},
+            {key: 'Header-Name', value: 'value2'}
+        ]);
+    });
+
+    it('should handle disabled and falsy header keys correctly', function () {
+        sinon.assert.calledWith(testrun.request.getCall(1), null);
+        sinon.assert.calledWith(testrun.response.getCall(1), null);
+
+        var response = testrun.response.getCall(1).args[2],
+            requestHeaders = JSON.parse(response.stream);
+
+        expect(requestHeaders).to.deep.include({
+            key: 'Header-Name-1',
+            value: 'value1'
+        });
+
+        expect(requestHeaders).to.not.deep.include({
+            key: 'Header-Name-2',
+            value: 'value2'
+        });
+
+        expect(requestHeaders).to.not.deep.include({
+            key: '',
+            value: 'value3'
+        });
+    });
+
+    it('should handle headers with different cases correctly', function () {
+        sinon.assert.calledWith(testrun.request.getCall(2), null);
+        sinon.assert.calledWith(testrun.response.getCall(2), null);
+
+        var response = testrun.response.getCall(2).args[2],
+            requestHeaders = JSON.parse(response.stream);
+
+        expect(requestHeaders).to.deep.include.members([
+            {key: 'Header-Name-0', value: 'value0'},
+            {key: 'header-name-1', value: 'value2'},
+            {key: 'HEADER-NAME-2', value: 'value3'}
+        ]);
+
+        // @todo: handle multiple headers with different capitalization
+        // https://github.com/postmanlabs/postman-app-support/issues/5372
+        expect(requestHeaders).to.not.deep.include({
+            key: 'Header-Name-1',
+            value: 'value1'
+        });
+    });
+});
