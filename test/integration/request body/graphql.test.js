2adc2c9aaca1f19164dd02b34dabf6e6f0769b3f
Udit Vasu <udit.vasu@getpostman.com>
2019-04-01 22:00:55 +0530

    Add tests for graphql body mode

--- test/integration/request body/graphql.test.js
+++ test/integration/request body/graphql.test.js
@@ -1,54 +1,282 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    server = require('../../fixtures/server');
 
 // @todo test with GraphQL server and add more tests
-describe('BodyTransformer: graphql', function () {
-    var testrun;
+describe('Request Body Mode: graphql', function () {
+    var testrun,
+        graphqlServer = server.createGraphQLServer({
+            schema: `
+                type Query {
+                    hello: String,
+                    square(n: Int!): Int
+                }
+            `,
+            root: {
+                hello: function () {
+                    return 'Hello world!';
+                },
+                square: function (args) {
+                    return args.n * args.n;
+                }
+            }
+        });
 
     before(function (done) {
-        this.run({
-            collection: {
-                item: [{
-                    request: {
-                        url: 'https://postman-echo.com/post',
-                        method: 'POST',
-                        body: {
-                            mode: 'graphql',
-                            graphql: {
-                                query: 'query Test { hello }',
-                                operationName: 'Test',
-                                variables: JSON.stringify({foo: 'bar'})
+        graphqlServer.listen(0, done);
+    });
+
+    after(function (done) {
+        graphqlServer.destroy(done);
+    });
+
+    describe('single query', function () {
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: graphqlServer.url,
+                            method: 'POST',
+                            body: {
+                                mode: 'graphql',
+                                graphql: {
+                                    query: '{ hello }'
+                                }
                             }
                         }
-                    }
-                }]
-            }
-        }, function (err, results) {
-            testrun = results;
-            done(err);
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun.done.getCall(0).args[0]).to.be.null;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true,
+                'response.calledOnce': true
+            });
+        });
+
+        it('should transform graphql request body correctly', function () {
+            var response = testrun.response.getCall(0).args[2],
+                responseBody = JSON.parse(response.stream.toString());
+
+            expect(response).to.have.property('code', 200);
+
+            expect(responseBody.request).to.have.property('headers');
+            expect(responseBody.request.headers).to.have.property('content-type', 'application/json');
+
+            expect(responseBody.request).to.have.property('body', '{"query":"{ hello }"}');
+            expect(responseBody.result).to.eql({
+                data: {hello: 'Hello world!'}
+            });
         });
     });
 
-    it('should have completed the run', function () {
-        expect(testrun).to.be.ok;
-        expect(testrun.done.getCall(0).args[0]).to.be.null;
-        expect(testrun).to.nested.include({
-            'done.calledOnce': true,
-            'start.calledOnce': true,
-            'request.calledOnce': true,
-            'response.calledOnce': true
+    describe('multiple queries', function () {
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: graphqlServer.url,
+                            method: 'POST',
+                            body: {
+                                mode: 'graphql',
+                                graphql: {
+                                    query: 'query Test { hello } query Test2 { hello }',
+                                    operationName: 'Test'
+                                }
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun.done.getCall(0).args[0]).to.be.null;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true,
+                'response.calledOnce': true
+            });
+        });
+
+        it('should transform graphql request body correctly', function () {
+            var response = testrun.response.getCall(0).args[2],
+                responseBody = JSON.parse(response.stream.toString());
+
+            expect(response).to.have.property('code', 200);
+
+            expect(responseBody.request).to.have.property('headers');
+            expect(responseBody.request.headers).to.have.property('content-type', 'application/json');
+
+            expect(responseBody.request).to.have.property('body',
+                '{"query":"query Test { hello } query Test2 { hello }","operationName":"Test"}');
+            expect(responseBody.result).to.eql({
+                data: {hello: 'Hello world!'}
+            });
         });
     });
 
-    it('should transform graphql request body correctly', function () {
-        var response = JSON.parse(testrun.response.getCall(0).args[2].stream.toString());
+    describe('empty query', function () {
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: graphqlServer.url,
+                            method: 'POST',
+                            body: {
+                                mode: 'graphql',
+                                graphql: {}
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
 
-        expect(response.headers).to.have.property('content-type', 'application/json');
-        expect(response.json).to.eql({
-            query: 'query Test { hello }',
-            operationName: 'Test',
-            variables: {
-                foo: 'bar'
-            }
+        it('should have completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun.done.getCall(0).args[0]).to.be.null;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true,
+                'response.calledOnce': true
+            });
+        });
+
+        it('should transform graphql request body correctly', function () {
+            var response = testrun.response.getCall(0).args[2],
+                responseBody = JSON.parse(response.stream.toString());
+
+            expect(response).to.have.property('code', 400);
+
+            expect(responseBody.request).to.have.property('headers');
+            expect(responseBody.request.headers).to.have.property('content-type', 'application/json');
+
+            expect(responseBody.request).to.have.property('body', '{"query":""}');
+            expect(responseBody.error).to.be.an('array').that.is.not.empty;
+        });
+    });
+
+    describe('variables', function () {
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: graphqlServer.url,
+                            method: 'POST',
+                            body: {
+                                mode: 'graphql',
+                                graphql: {
+                                    query: `
+                                        query Square($ten: Int!) {
+                                            square(n: $ten)
+                                        }
+                                    `,
+                                    variables: {ten: 10}
+                                }
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun.done.getCall(0).args[0]).to.be.null;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true,
+                'response.calledOnce': true
+            });
+        });
+
+        it('should transform graphql request body correctly', function () {
+            var response = testrun.response.getCall(0).args[2],
+                responseBody = JSON.parse(response.stream.toString());
+
+            expect(response).to.have.property('code', 200);
+
+            expect(responseBody.request).to.have.property('headers');
+            expect(responseBody.request.headers).to.have.property('content-type', 'application/json');
+
+            expect(responseBody.result).to.eql({
+                data: {square: 100}
+            });
+        });
+    });
+
+    describe('missing operationName', function () {
+        before(function (done) {
+            this.run({
+                collection: {
+                    item: [{
+                        request: {
+                            url: graphqlServer.url,
+                            method: 'POST',
+                            body: {
+                                mode: 'graphql',
+                                graphql: {
+                                    query: 'query Square($ten: Int!) { square(n: $ten) }' +
+                                        '\\n query Test { hello }',
+                                    variables: JSON.stringify({ten: 2})
+                                }
+                            }
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should have completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun.done.getCall(0).args[0]).to.be.null;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true,
+                'response.calledOnce': true
+            });
+        });
+
+        it('should transform graphql request body correctly', function () {
+            var response = testrun.response.getCall(0).args[2],
+                responseBody = JSON.parse(response.stream.toString());
+
+            expect(response).to.have.property('code', 400);
+
+            expect(responseBody.request).to.have.property('headers');
+            expect(responseBody.request.headers).to.have.property('content-type', 'application/json');
+
+            expect(responseBody.error).to.be.an('array').that.is.not.empty;
         });
     });
 });
