960fc017709875fe07d656461a32ce0dbdf511c6
Udit Vasu <udit.vasu@getpostman.com>
2019-06-08 00:23:44 +0530

    Add maxResponseSize requester option

--- README.md
+++ README.md
@@ -95,6 +95,9 @@ runner.run(collection, {
         // Maximum number of redirects to follow (only supported on Node, ignored in the browser)
         maxRedirects: 10,
 
+        // Maximum allowed response size in bytes (only supported on Node, ignored in the browser)
+        maxResponseSize: 1000000,
+
         // Removes the `referer` header when a redirect happens (only supported on Node, ignored in the browser)
         removeRefererHeaderOnRedirect: false,
 
---
+++ test/integration/requester-spec/maxResponseSize.test.js
@@ -0,0 +1,208 @@
+var sinon = require('sinon'),
+    expect = require('chai').expect;
+
+describe('Requester Spec: maxResponseSize', function () {
+    var testrun,
+        HOST = 'https://httpbin.org/bytes/10';
+
+    describe('with response > maxResponseSize', function () {
+        before(function (done) {
+            this.run({
+                requester: {
+                    maxResponseSize: 9
+                },
+                collection: {
+                    item: [{
+                        request: {
+                            url: HOST
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should complete the run', function () {
+            expect(testrun).to.be.ok;
+            sinon.assert.calledOnce(testrun.start);
+            sinon.assert.calledOnce(testrun.done);
+            sinon.assert.calledWith(testrun.done.getCall(0), null);
+        });
+
+        it('should abort the request', function () {
+            sinon.assert.calledOnce(testrun.request);
+            sinon.assert.calledOnce(testrun.response);
+            sinon.assert.calledOnce(testrun.responseStart);
+
+            sinon.assert.calledWith(testrun.responseStart.getCall(0), null);
+            expect(testrun.request.getCall(0).args[0]).to.have.property('message')
+                .that.equal('Maximum response size reached');
+            expect(testrun.response.getCall(0).args[0]).to.have.property('message')
+                .that.equal('Maximum response size reached');
+        });
+    });
+
+    describe('with response = maxResponseSize', function () {
+        before(function (done) {
+            this.run({
+                requester: {
+                    maxResponseSize: 10
+                },
+                collection: {
+                    item: [{
+                        request: {
+                            url: HOST
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should complete the run', function () {
+            expect(testrun).to.be.ok;
+            sinon.assert.calledOnce(testrun.start);
+            sinon.assert.calledOnce(testrun.done);
+            sinon.assert.calledWith(testrun.done.getCall(0), null);
+        });
+
+        it('should receive response body', function () {
+            sinon.assert.calledOnce(testrun.request);
+            sinon.assert.calledOnce(testrun.response);
+            sinon.assert.calledOnce(testrun.responseStart);
+
+            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            sinon.assert.calledWith(testrun.response.getCall(0), null);
+            sinon.assert.calledWith(testrun.responseStart.getCall(0), null);
+
+            var response = testrun.response.getCall(0).args[2];
+
+            expect(response.size()).to.have.property('body', 10);
+        });
+    });
+
+    describe('with response < maxResponseSize', function () {
+        before(function (done) {
+            this.run({
+                requester: {
+                    maxResponseSize: 100
+                },
+                collection: {
+                    item: [{
+                        request: {
+                            url: HOST
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should complete the run', function () {
+            expect(testrun).to.be.ok;
+            sinon.assert.calledOnce(testrun.start);
+            sinon.assert.calledOnce(testrun.done);
+            sinon.assert.calledWith(testrun.done.getCall(0), null);
+        });
+
+        it('should receive response body', function () {
+            sinon.assert.calledOnce(testrun.request);
+            sinon.assert.calledOnce(testrun.response);
+            sinon.assert.calledOnce(testrun.responseStart);
+
+            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            sinon.assert.calledWith(testrun.response.getCall(0), null);
+            sinon.assert.calledWith(testrun.responseStart.getCall(0), null);
+
+            var response = testrun.response.getCall(0).args[2];
+
+            expect(response.size()).to.have.property('body', 10);
+        });
+    });
+
+    describe('with maxResponseSize 0', function () {
+        before(function (done) {
+            this.run({
+                requester: {
+                    maxResponseSize: 0
+                },
+                collection: {
+                    item: [{
+                        request: {
+                            url: HOST
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should complete the run', function () {
+            expect(testrun).to.be.ok;
+            sinon.assert.calledOnce(testrun.start);
+            sinon.assert.calledOnce(testrun.done);
+            sinon.assert.calledWith(testrun.done.getCall(0), null);
+        });
+
+        it('should abort the request', function () {
+            sinon.assert.calledOnce(testrun.request);
+            sinon.assert.calledOnce(testrun.response);
+            sinon.assert.calledOnce(testrun.responseStart);
+
+            sinon.assert.calledWith(testrun.responseStart.getCall(0), null);
+            expect(testrun.request.getCall(0).args[0]).to.have.property('message')
+                .that.equal('Maximum response size reached');
+            expect(testrun.response.getCall(0).args[0]).to.have.property('message')
+                .that.equal('Maximum response size reached');
+        });
+    });
+
+    describe('with maxResponseSize Infinity', function () {
+        before(function (done) {
+            this.run({
+                requester: {
+                    maxResponseSize: Infinity
+                },
+                collection: {
+                    item: [{
+                        request: {
+                            url: HOST
+                        }
+                    }]
+                }
+            }, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should complete the run', function () {
+            expect(testrun).to.be.ok;
+            sinon.assert.calledOnce(testrun.start);
+            sinon.assert.calledOnce(testrun.done);
+            sinon.assert.calledWith(testrun.done.getCall(0), null);
+        });
+
+        it('should receive response body', function () {
+            sinon.assert.calledOnce(testrun.request);
+            sinon.assert.calledOnce(testrun.response);
+            sinon.assert.calledOnce(testrun.responseStart);
+
+            sinon.assert.calledWith(testrun.request.getCall(0), null);
+            sinon.assert.calledWith(testrun.response.getCall(0), null);
+            sinon.assert.calledWith(testrun.responseStart.getCall(0), null);
+
+            var response = testrun.response.getCall(0).args[2];
+
+            expect(response.size()).to.have.property('body', 10);
+        });
+    });
+});
