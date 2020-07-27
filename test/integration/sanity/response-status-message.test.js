846e6fe61574afe589feefacbf0f042a2de951cd
Appurva Murawat <appurva21@gmail.com>
2020-07-27 15:32:20 +0530

    Updated tests for status message

--- test/integration/sanity/response-status-message.test.js
+++ test/integration/sanity/response-status-message.test.js
@@ -1,53 +1,40 @@
-var server = require('../../fixtures/servers/_servers'),
-    expect = require('chai').expect;
+var expect = require('chai').expect;
 
 describe('response status message', function () {
-    var testrun,
-        rawEchoServer;
+    var testrun;
 
     before(function (done) {
-        var self = this;
-
-        rawEchoServer = server.createRawEchoServer();
-        rawEchoServer.listen(0, function () {
-            self.run({
-                collection: {
-                    item: [{
-                        request: {
-                            method: 'POST',
-                            url: rawEchoServer.url + '/raw-echo',
-                            body: {
-                                mode: 'raw',
-                                raw: 'HTTP/1.1 200 Working fine\r\n\r\n'
-                            }
+        this.run({
+            collection: {
+                item: [{
+                    request: {
+                        method: 'POST',
+                        url: global.servers.raw + '/raw-echo',
+                        body: {
+                            mode: 'raw',
+                            raw: 'HTTP/1.1 200 Working fine\r\n\r\n'
                         }
-                    }, {
-                        request: {
-                            method: 'POST',
-                            url: rawEchoServer.url + '/raw-echo',
-                            body: {
-                                mode: 'raw',
-                                raw: 'HTTP/1.1 200 Работает нормально\r\n\r\n'
-                            }
+                    }
+                }, {
+                    request: {
+                        method: 'POST',
+                        url: global.servers.raw + '/raw-echo',
+                        body: {
+                            mode: 'raw',
+                            raw: 'HTTP/1.1 200 Работает нормально\r\n\r\n'
                         }
-                    }]
-                }
-            }, function (err, results) {
-                testrun = results;
+                    }
+                }]
+            }
+        }, function (err, results) {
+            testrun = results;
 
-                done(err);
-            });
+            done(err);
         });
-    });
-
-    after(function () {
-        rawEchoServer.destroy();
     });
 
     it('should have completed the run', function () {
         expect(testrun).to.be.ok;
-        expect(testrun.request.getCall(0)).to.nested.include({'args[0]': null, 'args[2].code': 200});
-        expect(testrun.request.getCall(1)).to.nested.include({'args[0]': null, 'args[2].code': 200});
         expect(testrun).to.nested.include({
             'done.calledOnce': true,
             'start.calledOnce': true
@@ -55,10 +42,12 @@ describe('response status message', function () {
     });
 
     it('should be decoded correctly containing characters in range of latin1 encoding', function () {
+        expect(testrun.request.getCall(0)).to.nested.include({'args[0]': null, 'args[2].code': 200});
         expect(testrun.request.getCall(0).args[2]).to.have.property('status', 'Working fine');
     });
 
     it('should be decoded correctly containing characters in range of utf8 encoding', function () {
+        expect(testrun.request.getCall(1)).to.nested.include({'args[0]': null, 'args[2].code': 200});
         expect(testrun.request.getCall(1).args[2]).to.have.property('status', 'Работает нормально');
     });
 });
