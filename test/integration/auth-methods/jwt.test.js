f2bdaad362ffe64608eb9d474c3a74eba08cd1bb
Sathishkumar Thangavel <sathishkumar.thangavel@postman.com>
2022-12-05 15:58:04 +0530

    Add jwt payload generation and unit test

--- test/integration/auth-methods/jwt.test.js
+++ test/integration/auth-methods/jwt.test.js
@@ -1,5 +1,9 @@
 const expect = require('chai').expect,
-    AUTHORIZATION_HEADER = 'authorizationHeader';
+    jwt = require('jsonwebtoken'),
+    Header = require('postman-collection').Header,
+    QueryParam = require('postman-collection').QueryParam,
+    AUTHORIZATION_HEADER = 'authorizationHeader',
+    QUERY_PARAM = 'queryParam';
 
 describe('jwt auth', function () {
     let testrun;
@@ -23,14 +27,6 @@ describe('jwt auth', function () {
                             }
                         }
                     }
-                },
-                environment: {
-                    values: [
-                        {
-                            key: 'token',
-                            value: 'ABC123'
-                        }
-                    ]
                 }
             };
 
@@ -56,7 +52,7 @@ describe('jwt auth', function () {
             request.headers.members.forEach(function (header) {
                 headers.push(header.key);
             });
-            expect(headers).to.not.have.members(['Authorization']);
+            expect(headers).that.does.not.include('Authorization');
         });
     });
 
@@ -79,14 +75,6 @@ describe('jwt auth', function () {
                             }
                         }
                     }
-                },
-                environment: {
-                    values: [
-                        {
-                            key: 'token',
-                            value: 'ABC123'
-                        }
-                    ]
                 }
             };
 
@@ -112,7 +100,7 @@ describe('jwt auth', function () {
             request.headers.members.forEach(function (header) {
                 headers.push(header.key);
             });
-            expect(headers).to.not.have.members(['Authorization']);
+            expect(headers).that.does.not.include('Authorization');
         });
     });
 
@@ -135,12 +123,63 @@ describe('jwt auth', function () {
                             }
                         }
                     }
+                }
+            };
+
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true
+            });
+        });
+
+        it('should not add Authorization header', function () {
+            const headers = [],
+                request = testrun.request.firstCall.args[3];
+
+            request.headers.members.forEach(function (header) {
+                headers.push(header.key);
+            });
+            expect(headers).that.does.not.include('Authorization');
+        });
+    });
+
+    // TODO: jwt header test for different algorithms
+
+
+    describe('with valid payload for HS256 algorithm and token add to Authorization header', function () {
+        before(function (done) {
+            const runOptions = {
+                collection: {
+                    item: {
+                        request: {
+                            url: 'https://postman-echo.com/headers',
+                            auth: {
+                                type: 'jwt',
+                                jwt: {
+                                    algorithm: 'HS256',
+                                    header: [{ key: 'typ', value: 'JWT' }],
+                                    payload: [{ key: 'test', value: '123' }, { key: 'name', value: 'demo-name' }],
+                                    secretOrPrivateKey: '{{secretOrPrivateKey}}',
+                                    tokenAddTo: AUTHORIZATION_HEADER
+                                }
+                            }
+                        }
+                    }
                 },
                 environment: {
                     values: [
                         {
-                            key: 'token',
-                            value: 'ABC123'
+                            key: 'secretOrPrivateKey',
+                            value: 'cdVGscbTozmMseA6c7YfhimIF8seD02mN0g4x'
                         }
                     ]
                 }
@@ -161,14 +200,187 @@ describe('jwt auth', function () {
             });
         });
 
-        it('should not add Authorization header', function () {
-            const members = [],
-                request = testrun.request.firstCall.args[3];
+        it('should add Authorization header with bearer as jwt token', function () {
+            const headers = [],
+                request = testrun.request.firstCall.args[3],
+                response = testrun.request.firstCall.args[2];
+
+            let jwtToken;
 
             request.headers.members.forEach(function (header) {
-                members.push(header.key);
+                if (header.key === 'Authorization') {
+                    jwtToken = header.value.split('Bearer ')[1];
+                }
+                headers.push(header.key);
             });
-            expect(members).to.not.have.members(['Authorization']);
+
+            expect(request.headers.members).to.include.deep.members([
+                new Header({ key: 'Authorization', value: `Bearer ${jwtToken}`, system: true })
+            ]);
+
+            expect(response.json()).to.nested.include({
+                'headers.authorization': `Bearer ${jwtToken}`
+            });
+
+            expect(jwt.verify(jwtToken, 'cdVGscbTozmMseA6c7YfhimIF8seD02mN0g4x'))
+                .to.be.deep.equal({ test: '123', name: 'demo-name' });
+            expect(jwt.decode(jwtToken, { complete: true }).header)
+                .to.be.deep.equal({ alg: 'HS256', typ: 'JWT' });
+        });
+    });
+
+    describe('with valid payload for HS384 algorithm and token add to url query param', function () {
+        before(function (done) {
+            const runOptions = {
+                collection: {
+                    item: {
+                        request: {
+                            url: 'https://postman-echo.com/get',
+                            auth: {
+                                type: 'jwt',
+                                jwt: {
+                                    algorithm: 'HS384',
+                                    header: [{ key: 'typ', value: 'JWT' }],
+                                    payload: [{ key: 'name', value: '{{name}}' }],
+                                    secretOrPrivateKey: '{{secretOrPrivateKey}}',
+                                    tokenAddTo: QUERY_PARAM,
+                                    queryParamKey: 'jwt'
+                                }
+                            }
+                        }
+                    }
+                },
+                environment: {
+                    values: [
+                        {
+                            key: 'name',
+                            value: 'test-name'
+                        },
+                        {
+                            key: 'secretOrPrivateKey',
+                            value: 'cdVGscbTozmMseA6c7YfhimIF8seD02mN0g4x1234567891'
+                        }
+                    ]
+                }
+            };
+
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true
+            });
+        });
+
+        it('should generate a valid jwt token & add to query param correctly', function () {
+            const queries = [],
+                request = testrun.request.firstCall.args[3],
+                response = testrun.request.firstCall.args[2];
+
+            let jwtToken;
+
+            request.url.query.members.forEach(function (query) {
+                if (query.key === 'jwt') {
+                    jwtToken = query.value;
+                }
+                queries.push(query.value);
+            });
+
+            expect(request.url.query.members).to.include.deep.members([
+                new QueryParam({ key: 'jwt', value: jwtToken })
+            ]);
+
+            expect(jwt.verify(jwtToken, 'cdVGscbTozmMseA6c7YfhimIF8seD02mN0g4x1234567891'))
+                .to.be.deep.equal({ name: 'test-name' });
+            expect(jwt.decode(jwtToken, { complete: true }).header)
+                .to.be.deep.equal({ alg: 'HS384', typ: 'JWT' });
+
+            expect(response.json()).to.nested.include({
+                'args.jwt': jwtToken
+            });
+        });
+    });
+
+    describe('with valid payload for HS512 algorithm and token add to Authorization header', function () {
+        const secretKey = 'cdVGscbTozmMseA6c7YfhimIF8seD02mN0g4x1212121232323434242352352345252245245245245241212s';
+
+        before(function (done) {
+            const runOptions = {
+                collection: {
+                    item: {
+                        request: {
+                            url: 'https://postman-echo.com/headers',
+                            auth: {
+                                type: 'jwt',
+                                jwt: {
+                                    algorithm: 'HS512',
+                                    header: [{ key: 'typ', value: 'JWT' }],
+                                    payload: [{ key: 'test', value: '123' }, { key: 'name', value: 'demo-name' }],
+                                    secretOrPrivateKey: '{{secretOrPrivateKey}}',
+                                    tokenAddTo: AUTHORIZATION_HEADER
+                                }
+                            }
+                        }
+                    }
+                },
+                environment: {
+                    values: [
+                        {
+                            key: 'secretOrPrivateKey',
+                            value: secretKey
+                        }
+                    ]
+                }
+            };
+
+            this.run(runOptions, function (err, results) {
+                testrun = results;
+                done(err);
+            });
+        });
+
+        it('should completed the run', function () {
+            expect(testrun).to.be.ok;
+            expect(testrun).to.nested.include({
+                'done.calledOnce': true,
+                'start.calledOnce': true,
+                'request.calledOnce': true
+            });
+        });
+
+        it('should add Authorization header with bearer as jwt token', function () {
+            const headers = [],
+                request = testrun.request.firstCall.args[3],
+                response = testrun.request.firstCall.args[2];
+
+            let jwtToken;
+
+            request.headers.members.forEach(function (header) {
+                if (header.key === 'Authorization') {
+                    jwtToken = header.value.split('Bearer ')[1];
+                }
+                headers.push(header.key);
+            });
+
+            expect(request.headers.members).to.include.deep.members([
+                new Header({ key: 'Authorization', value: `Bearer ${jwtToken}`, system: true })
+            ]);
+
+            expect(response.json()).to.nested.include({
+                'headers.authorization': `Bearer ${jwtToken}`
+            });
+
+            expect(jwt.verify(jwtToken, secretKey))
+                .to.be.deep.equal({ test: '123', name: 'demo-name' });
+            expect(jwt.decode(jwtToken, { complete: true }).header)
+                .to.be.deep.equal({ alg: 'HS512', typ: 'JWT' });
         });
     });
 });
