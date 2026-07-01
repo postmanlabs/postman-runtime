066b59d56daa36c60cd8e35f45ae5e2131527ad5
Devesh Kumar <devesh.kumar@postman.com>
2026-07-01 17:13:15 +0530

    Remove unnecessary parts of the postman echo server

--- test/fixtures/servers/postmanEcho.js
+++ test/fixtures/servers/postmanEcho.js
@@ -1,15 +1,9 @@
-const _ = require('lodash'),
-    http = require('http'),
+const http = require('http'),
     http2 = require('http2'),
-    crypto = require('crypto'),
-    zlib = require('zlib'),
-    querystring = require('querystring'),
     enableServerDestroy = require('server-destroy'),
-    Hawk = require('postman-request/lib/hawk'),
 
     server = require('./_servers'),
 
-    DATA_URI_PREFIX = 'data:application/octet-stream;base64,',
     ECHO_INTERNAL_HEADERS = [
         'x-postman-echo-original-url',
         'x-postman-echo-original-protocol',
@@ -23,23 +17,7 @@ const _ = require('lodash'),
         'x-forwarded-client-cert',
         'x-request-id'
     ],
-    OMIT_PREFIXES = ['cf-', 'x-envoy', 'x-b3'],
-    BASIC_USERS = {
-        postman: 'password',
-        testuser: 'testpass'
-    },
-    HAWK_CREDENTIALS = {
-        dh37fgj492je: {
-            id: 'dh37fgj492je',
-            key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
-            algorithm: 'sha256'
-        },
-        testid: {
-            id: 'testid',
-            key: 'testkey',
-            algorithm: 'sha256'
-        }
-    };
+    OMIT_PREFIXES = ['cf-', 'x-envoy', 'x-b3'];
 
 let httpServer,
     httpsServer;
@@ -67,10 +45,19 @@ function lowerHeaders (headers) {
 }
 
 function echoHeaders (req) {
-    return _.omitBy(lowerHeaders(req.headers), function (value, key) {
-        return ECHO_INTERNAL_HEADERS.includes(key) || OMIT_HEADERS.includes(key) ||
-            OMIT_PREFIXES.some(function (prefix) { return key.startsWith(prefix); });
+    const headers = lowerHeaders(req.headers);
+
+    ECHO_INTERNAL_HEADERS.concat(OMIT_HEADERS).forEach(function (key) {
+        delete headers[key];
     });
+
+    Object.keys(headers).forEach(function (key) {
+        if (OMIT_PREFIXES.some(function (prefix) { return key.startsWith(prefix); })) {
+            delete headers[key];
+        }
+    });
+
+    return headers;
 }
 
 function originalUrl (req) {
@@ -90,12 +77,7 @@ function queryArgs (req) {
 
     parsed.searchParams.forEach(function (value, key) {
         if (Object.hasOwn(args, key)) {
-            if (Array.isArray(args[key])) {
-                args[key].push(value);
-            }
-            else {
-                args[key] = [args[key], value];
-            }
+            args[key] = Array.isArray(args[key]) ? args[key].concat(value) : [args[key], value];
 
             return;
         }
@@ -127,260 +109,16 @@ function parseCookies (header) {
     return cookies;
 }
 
-function parseMultipart (contentType, body) {
-    const boundaryMatch = (/boundary=(?:"([^"]+)"|([^;]+))/i).exec(contentType),
-        form = {},
-        files = {},
-        boundary = boundaryMatch && '--' + (boundaryMatch[1] || boundaryMatch[2]),
-        text = body.toString('binary');
-
-    if (!boundaryMatch) {
-        return { form, files };
-    }
-
-    text.split(boundary).forEach(function (part) {
-        if (!part || part === '--\r\n' || part === '--') { return; }
-
-        part = part.replace((/^\r\n/), '').replace((/\r\n--$/), '');
-
-        const headerEnd = part.indexOf('\r\n\r\n'),
-            rawHeaders = part.slice(0, headerEnd),
-            content = part.slice(headerEnd + 4).replace((/\r\n$/), ''),
-            nameMatch = (/name="([^"]*)"/i).exec(rawHeaders),
-            filenameMatch = (/filename="([^"]*)"/i).exec(rawHeaders);
-
-        if (headerEnd === -1) { return; }
-
-        if (!nameMatch) { return; }
-
-        if (filenameMatch) {
-            files[filenameMatch[1]] = DATA_URI_PREFIX + Buffer.from(content, 'binary').toString('base64');
-
-            return;
-        }
-
-        if (Object.hasOwn(form, nameMatch[1])) {
-            if (Array.isArray(form[nameMatch[1]])) {
-                form[nameMatch[1]].push(content);
-            }
-            else {
-                form[nameMatch[1]] = [form[nameMatch[1]], content];
-            }
-
-            return;
-        }
-
-        form[nameMatch[1]] = content;
-    });
-
-    return { form, files };
-}
-
-function parseBody (req, body) {
-    const contentType = req.headers['content-type'] || '';
-
-    if ((/multipart\/form-data/i).test(contentType)) {
-        const parsed = parseMultipart(contentType, body);
-
-        return {
-            data: '',
-            files: parsed.files,
-            form: parsed.form,
-            json: Object.keys(parsed.form).length ? parsed.form : null
-        };
-    }
-
-    if ((/application\/x-www-form-urlencoded/i).test(contentType)) {
-        const form = querystring.parse(body.toString());
-
-        return {
-            data: '',
-            files: {},
-            form: form,
-            json: Object.keys(form).length ? form : null
-        };
-    }
-
-    if ((/application\/octet-stream/i).test(contentType)) {
-        return {
-            data: body,
-            files: {},
-            form: {},
-            json: null
-        };
-    }
-
-    if ((/application\/json/i).test(contentType)) {
-        if (!body.length) {
-            return { data: '', files: {}, form: {}, json: null };
-        }
-
-        try {
-            const json = JSON.parse(body.toString());
-
-            return { data: json, files: {}, form: {}, json: json };
-        }
-        catch (e) {
-            return { data: body.toString(), files: {}, form: {}, json: null };
-        }
-    }
-
-    if (!contentType && body.length) {
-        try {
-            JSON.parse(body.toString());
-
-            return {
-                data: body,
-                files: {},
-                form: {},
-                json: null
-            };
-        }
-        catch (e) {
-            return {
-                data: body.toString(),
-                files: {},
-                form: {},
-                json: null
-            };
-        }
-    }
-
-    if (!contentType && !body.length) {
-        return {
-            data: {},
-            files: {},
-            form: {},
-            json: null
-        };
-    }
-
-    return {
-        data: body.toString(),
-        files: {},
-        form: {},
-        json: null
-    };
-}
-
 function methodResponse (req, body) {
     return {
         args: queryArgs(req),
-        ...parseBody(req, body),
+        data: body.length ? body.toString() : {},
+        files: {},
+        form: {},
+        json: null,
         headers: echoHeaders(req),
         url: originalUrl(req)
     };
-}
-
-function verifyBasicAuth (req) {
-    const authorization = req.headers.authorization;
-    let credentials,
-        index;
-
-    if (!authorization || !authorization.startsWith('Basic ')) {
-        return false;
-    }
-
-    credentials = Buffer.from(authorization.slice(6), 'base64').toString();
-    index = credentials.indexOf(':');
-
-    if (index === -1) { return false; }
-
-    return BASIC_USERS[credentials.slice(0, index)] === credentials.slice(index + 1);
-}
-
-function md5 (value) {
-    return crypto.createHash('md5').update(value).digest('hex');
-}
-
-function parseAuthParams (authorization, prefix) {
-    const params = {};
-
-    authorization.slice(prefix.length).replace((/(\w+)=(?:"([^"]*)"|([^,\s]+))/g),
-        function (match, key, quoted, bare) {
-            params[key] = quoted || bare;
-        });
-
-    return params;
-}
-
-function verifyDigestAuth (req) {
-    const authorization = req.headers.authorization;
-    let params,
-        password,
-        uri,
-        ha1,
-        ha2;
-
-    if (!authorization || !authorization.startsWith('Digest ')) {
-        return false;
-    }
-
-    params = parseAuthParams(authorization, 'Digest ');
-    password = BASIC_USERS[params.username];
-    uri = new URL(originalUrl(req)).pathname + new URL(originalUrl(req)).search;
-    ha1 = md5(params.username + ':' + params.realm + ':' + password);
-    ha2 = md5(req.method + ':' + uri);
-
-    if (!password || params.realm !== 'Users' || params.uri !== uri) {
-        return false;
-    }
-
-    return params.response === md5([ha1, params.nonce, params.nc, params.cnonce, params.qop, ha2].join(':'));
-}
-
-function verifyOAuth1 (req) {
-    const authorization = req.headers.authorization,
-        parsedUrl = new URL(originalUrl(req)),
-        hasOAuthHeader = authorization && authorization.startsWith('OAuth ');
-
-    return Boolean(hasOAuthHeader || parsedUrl.searchParams.get('oauth_signature'));
-}
-
-function verifyHawk (req, body) {
-    const authorization = req.headers.authorization;
-    let params,
-        credentials,
-        parsedUrl,
-        port;
-
-    if (!authorization || !authorization.startsWith('Hawk ')) {
-        return false;
-    }
-
-    params = parseAuthParams(authorization, 'Hawk ');
-    credentials = HAWK_CREDENTIALS[params.id];
-    parsedUrl = new URL(originalUrl(req));
-    port = parsedUrl.port || (parsedUrl.protocol === 'https:' ? 443 : 80);
-
-    if (!credentials) { return false; }
-
-    if (params.hash) {
-        const hash = crypto.createHash('sha256');
-
-        hash.update('hawk.1.payload\n');
-        hash.update((req.headers['content-type'] || '').split(';')[0].trim().toLowerCase());
-        hash.update('\n');
-        hash.update(body);
-        hash.update('\n');
-
-        if (hash.digest('base64') !== params.hash) {
-            return false;
-        }
-    }
-
-    return params.mac === Hawk.calculateMac(credentials, {
-        ts: params.ts,
-        nonce: params.nonce,
-        method: req.method,
-        resource: parsedUrl.pathname + parsedUrl.search,
-        host: parsedUrl.hostname,
-        port: port,
-        hash: params.hash,
-        ext: params.ext,
-        app: params.app,
-        dlg: params.dlg
-    });
 }
 
 function redirect (res, location, headers) {
@@ -423,10 +161,6 @@ function handle (req, res) {
             return sendJSON(res, 200, methodResponse(req, body));
         }
 
-        if (pathname === '/headers') {
-            return sendJSON(res, 200, { headers: echoHeaders(req) });
-        }
-
         if (pathname === '/cookies' || pathname === '/cookies/get') {
             return sendJSON(res, 200, { cookies: parseCookies(req.headers.cookie) });
         }
@@ -449,137 +183,6 @@ function handle (req, res) {
             });
 
             return redirect(res, '/cookies', { 'set-cookie': cookies });
-        }
-
-        if (pathname === '/redirect-to') {
-            const location = parsedUrl.searchParams.get('url');
-
-            if (!location) {
-                return sendJSON(res, 400, { status: 400, message: 'No redirect url provided' });
-            }
-
-            return redirect(res, location);
-        }
-
-        if (pathname.startsWith('/status/')) {
-            const status = Number(pathname.split('/')[2]);
-
-            if (!http.STATUS_CODES[status]) {
-                return sendJSON(res, 400, {
-                    status: 400,
-                    message: 'Invalid HTTP Status Code',
-                    moreinfo: 'https://www.wikiwand.com/en/List_of_HTTP_status_codes'
-                });
-            }
-
-            return sendJSON(res, status, { status });
-        }
-
-        if (pathname === '/response-headers') {
-            const headers = {},
-                args = {};
-
-            parsedUrl.searchParams.forEach(function (value, key) {
-                if (Object.hasOwn(headers, key)) {
-                    headers[key] = Array.isArray(headers[key]) ? headers[key].concat(value) : [headers[key], value];
-                }
-                else {
-                    headers[key] = value;
-                }
-
-                args[key] = value;
-            });
-
-            return sendJSON(res, 200, args, headers);
-        }
-
-        if (pathname === '/gzip') {
-            const payload = Buffer.from(JSON.stringify({
-                gzipped: true,
-                headers: echoHeaders(req),
-                method: 'GET'
-            }, null, 2), 'utf8');
-
-            res.writeHead(200, {
-                'content-encoding': 'gzip',
-                'content-type': 'application/json'
-            });
-
-            return res.end(zlib.gzipSync(payload));
-        }
-
-        if (pathname.startsWith('/delay/')) {
-            const seconds = Math.min(Number(pathname.split('/')[2]) || 0, 10);
-
-            return setTimeout(function () {
-                sendJSON(res, 200, { delay: String(seconds) });
-            }, seconds * 1000);
-        }
-
-        if (pathname.startsWith('/bytes/')) {
-            const parts = pathname.split('/'),
-                value = Number(parts[2]) || 0,
-                multiplier = { kb: 1024, mb: 1024 * 1024, gb: 1024 * 1024 * 1024 }[parts[3]] || 1,
-                size = Math.min(value * multiplier, 1024 * 1024 * 1024);
-
-            res.writeHead(200, { 'content-type': 'text/plain' });
-
-            return res.end('a'.repeat(size));
-        }
-
-        if (pathname === '/type/xml') {
-            res.writeHead(200, { 'content-type': 'application/xml; charset=utf-8' });
-
-            return res.end('<?xml version="1.0" encoding="utf-8"?><food><key>Homestyle Breakfast</key>' +
-                '<value>950</value></food>');
-        }
-
-        if (pathname === '/type/html') {
-            res.writeHead(200, { 'content-type': 'application/html; charset=utf-8' });
-
-            return res.end('<!DOCTYPE html><html><head><title>Hello World!</title></head><body>' +
-                '<h1>Hello World!</h1></body></html>');
-        }
-
-        if (pathname === '/basic-auth') {
-            if (!verifyBasicAuth(req)) {
-                res.writeHead(401, { 'www-authenticate': 'Basic realm="Users"' });
-
-                return res.end('Unauthorized');
-            }
-
-            return sendJSON(res, 200, { authenticated: true });
-        }
-
-        if (pathname === '/digest-auth') {
-            if (!verifyDigestAuth(req)) {
-                res.writeHead(401, {
-                    'www-authenticate': 'Digest realm="Users", qop="auth", algorithm="MD5", nonce="md5nonce"'
-                });
-
-                return res.end('Unauthorized');
-            }
-
-            return sendJSON(res, 200, { authenticated: true });
-        }
-
-        if (pathname === '/oauth1') {
-            return sendJSON(res, verifyOAuth1(req) ? 200 : 401, verifyOAuth1(req) ? {
-                status: 'pass',
-                message: 'OAuth-1.0a signature verification was successful'
-            } : { error: 'Invalid signature' });
-        }
-
-        if (pathname === '/oauth2') {
-            return sendJSON(res, req.headers.authorization ? 200 : 401, req.headers.authorization ? {
-                authenticated: true
-            } : { error: 'Unauthorized' });
-        }
-
-        if (pathname === '/auth/hawk') {
-            return sendJSON(res, verifyHawk(req, body) ? 200 : 401, verifyHawk(req, body) ? {
-                message: 'Hawk Authentication Successful'
-            } : { message: 'Hawk Authentication Failed' });
         }
 
         return sendJSON(res, 404, { status: 404, message: 'Not Found' });
